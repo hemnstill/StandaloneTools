209b2804362db4970b4fdfd41891c4ea31729a92
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 02:15:25 +0400

    musl-dev

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,7 +3,7 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
+apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake musl-dev git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
