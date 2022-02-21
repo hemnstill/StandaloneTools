b51c5c36bf9ec93905a2697e9ce6fbb0da7d1b76
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-02-22 00:12:22 +0400

    static bsdtar 3.5.1 (libarchive) with zlib and bzip2 (#1)

---
+++ bsdtar/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+apk update
+apk add --no-cache alpine-sdk zlib-dev bzip2-dev zlib-static bzip2-static
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+
+# Download release
+wget https://github.com/libarchive/libarchive/releases/download/v3.5.1/libarchive-3.5.1.tar.gz -O libarchive-3.5.1.tar.gz
+tar -xf libarchive-3.5.1.tar.gz && cd libarchive-3.5.1
+
+./configure LDFLAGS='--static' --enable-bsdtar=static --disable-shared --disable-bsdcpio --disable-bsdcat
+make -j$(nproc)
+gcc -static -o ../bsdtar \
+  tar/bsdtar-bsdtar.o \
+  tar/bsdtar-cmdline.o \
+  tar/bsdtar-creation_set.o \
+  tar/bsdtar-read.o \
+  tar/bsdtar-subst.o \
+  tar/bsdtar-util.o \
+  tar/bsdtar-write.o \
+  .libs/libarchive.a \
+  .libs/libarchive_fe.a \
+  /lib/libz.a \
+  /usr/lib/libbz2.a
+
+cd "$dp0/release"
+strip "bsdtar"
+chmod +x "bsdtar"
+"./bsdtar" --version
