9ebeb3c75842f84721ef68136c760786cf0cec32
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 02:16:52 +0400

    linux-headers

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,8 +3,9 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake musl-dev git
+apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
+apk add --no-cache musl-dev linux-headers
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
