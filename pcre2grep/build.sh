4fa677db472e0bf4793dbc1fca763d5264c34fd4
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-02-22 01:12:38 +0400

    pcre2grep 10.39 (#2)

---
+++ pcre2grep/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+apk update
+apk add --no-cache alpine-sdk pcre2-dev
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+
+# Download release
+wget https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.39/pcre2-10.39.tar.gz -O pcre2-10.39.tar.gz
+tar -xf pcre2-10.39.tar.gz && cd pcre2-10.39
+
+./configure LDFLAGS='--static' --disable-shared --enable-jit --enable-pcre2-8 --enable-pcre2-16 --enable-pcre2-32
+
+make -j$(nproc)
+
+cp -f "$dp0/release/pcre2-10.39/pcre2grep" "$dp0/release/"
+
+cd "$dp0/release"
+strip "pcre2grep"
+chmod +x "pcre2grep"
+"./pcre2grep" --version
