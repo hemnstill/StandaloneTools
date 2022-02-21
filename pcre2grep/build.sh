3b18fa98e432da93d1b8ec5d1f45abad4b0ff8b4
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 00:28:39 +0400

    pcre2grep make

--- pcre2grep/build.sh
+++ pcre2grep/build.sh
@@ -11,4 +11,6 @@ mkdir -p "$dp0/release" && cd "$dp0/release"
 wget https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.39/pcre2-10.39.tar.gz -O pcre2-10.39.tar.gz
 tar -xf pcre2-10.39.tar.gz && cd pcre2-10.39
 
-./configure LDFLAGS='--static' --enable-bsdtar=static --disable-shared --disable-bsdcpio --disable-bsdcat
+./configure LDFLAGS='--static' --enable-jit --enable-pcre2-8 --enable-pcre2-16 --enable-pcre2-32
+
+make -j$(nproc)
