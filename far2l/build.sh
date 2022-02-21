32c5883e999d3a58eede99c09551674b228abbcc
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 01:56:19 +0400

    +x

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,7 +3,7 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk pcre2-dev
+apk add --no-cache alpine-sdk gawk m4 pcre2-dev libxerces-c-dev libspdlog-dev libuchardet-dev libssh-dev libssl-dev libsmbclient-dev libnfs-dev libneon27-dev libarchive-dev cmake git
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
@@ -11,13 +11,4 @@ mkdir -p "$dp0/release" && cd "$dp0/release"
 wget https://github.com/elfmz/far2l/archive/refs/tags/v_2.4.0.tar.gz -O v_2.4.0.tar.gz
 tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
-#./configure LDFLAGS='--static' --disable-shared --enable-jit --enable-pcre2-8 --enable-pcre2-16 --enable-pcre2-32
-#
-#make -j$(nproc)
-#
-#cp -f "$dp0/release/pcre2-10.39/pcre2grep" "$dp0/release/"
-#
-#cd "$dp0/release"
-#strip "pcre2grep"
-#chmod +x "pcre2grep"
-#"./pcre2grep" --version
+cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release ..
