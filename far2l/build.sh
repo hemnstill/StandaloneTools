7300225e9a14a8180a75b94c70f5cf782db719fc
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 02:21:52 +0400

    libexecinfo-dev

--- far2l/build.sh
+++ far2l/build.sh
@@ -5,7 +5,7 @@ set -e
 apk update
 apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
-apk add --no-cache musl-dev linux-headers
+apk add --no-cache musl-dev linux-headers libexecinfo-dev
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
