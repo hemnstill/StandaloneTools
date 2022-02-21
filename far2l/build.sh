df840a2ccb5c0a2908243801a37c01c58e8e4cec
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 01:59:57 +0400

    - alpine packages

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,7 +3,7 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk gawk m4 pcre2-dev libxerces-c-dev libspdlog-dev libuchardet-dev libssh-dev libssl-dev libsmbclient-dev libnfs-dev libneon27-dev libarchive-dev cmake git
+apk add --no-cache alpine-sdk gawk m4 pcre2-dev libssh-dev libressl-dev libnfs-dev libarchive-dev cmake git
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
@@ -11,4 +11,4 @@ mkdir -p "$dp0/release" && cd "$dp0/release"
 wget https://github.com/elfmz/far2l/archive/refs/tags/v_2.4.0.tar.gz -O v_2.4.0.tar.gz
 tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
-cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release ..
+cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release .
