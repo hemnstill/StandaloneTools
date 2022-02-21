4b933604e0a001ee979470e898e71a95640f6a2d
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-21 23:13:05 +0400

    upload

--- bsdtar/build.sh
+++ bsdtar/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 dp0="$(realpath "$(dirname "$0")")"
-mkdir -p "$dp0/release" && cd "$dp0/release" || exit 1
-
 set -e
 
-# Install build dependencies
 apk update
 apk add --no-cache alpine-sdk util-linux strace file zlib-dev bzip2-dev autoconf automake libtool zlib-static
 
+mkdir -p "$dp0/release" && cd "$dp0/release"
+
+# Download release
 wget https://github.com/libarchive/libarchive/releases/download/v3.5.1/libarchive-3.5.1.tar.gz -O libarchive-3.5.1.tar.gz
 tar -xf libarchive-3.5.1.tar.gz && cd libarchive-3.5.1
 
-./configure LDFLAGS='--static' --enable-bsdtar=static --disable-shared --with-zlib --without-bz2lib
+./configure LDFLAGS='--static' --enable-bsdtar=static --disable-shared --with-zlib --without-bz2lib --disable-bsdcpio --disable-bsdcat
 make -j$(nproc)
-gcc -static -o bsdtar \
+gcc -static -o ../bsdtar \
   tar/bsdtar-bsdtar.o \
   tar/bsdtar-cmdline.o \
   tar/bsdtar-creation_set.o \
