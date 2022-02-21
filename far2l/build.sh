ba0220d055796dc92a7e086f8396cbe0a1877236
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 02:12:37 +0400

    --build

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,8 +3,8 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk gawk m4 pcre-dev pcre2-dev libssh-dev libressl-dev libnfs-dev libarchive-dev cmake git
-apk add --no-cache uchardet-dev neon-dev spdlog-dev xerces-c-dev
+apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
+apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
@@ -13,3 +13,5 @@ wget https://github.com/elfmz/far2l/archive/refs/tags/v_2.4.0.tar.gz -O v_2.4.0.
 tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
 cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release .
+
+cmake --build .
