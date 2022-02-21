17616be16db468943d9f517e83fa65d540bf41f8
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 03:22:19 +0400

    -glibc

--- far2l/build.sh
+++ far2l/build.sh
@@ -7,10 +7,6 @@ apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarch
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
 apk add --no-cache musl-dev linux-headers libexecinfo-dev libexecinfo-static
 
-wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.34-r0/glibc-2.34-r0.apk
-apk add --no-cache glibc-2.34-r0.apk
-
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
 # Download release
