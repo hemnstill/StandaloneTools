8fa5b6c41174d8b316c8fa0736ba330f6e4fe462
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-23 00:47:41 +0400

    g++

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,7 +3,7 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake git
+apk add --no-cache gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
 apk add --no-cache musl-dev linux-headers libexecinfo-dev libexecinfo-static uchardet-static
 
@@ -15,7 +15,7 @@ tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
 cp -f "../SafeMMap.cpp" "./far2l/src/base/"
 
-cmake -DUSEWX=no -DNETROCKS=no -DUSEUCD=no -DCOLORER=no -DCMAKE_BUILD_TYPE=Release .
+cmake -DUSEWX=no -DNETROCKS=no -DUSEUCD=no -DCOLORER=no -DMULTIARC=no -DCMAKE_BUILD_TYPE=Release .
 
 cmake --build . --config Release
 
