0d79518ee36ef89b48bc036a6baaf71256c4f6ad
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-22 22:16:51 +0400

    far2l_deb/build.sh

---
+++ far2l_deb/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+apt-get install -y libspdlog-dev patchelf wget gawk m4 libx11-dev libxi-dev libxerces-c-dev libuchardet-dev libssh-dev libssl-dev libnfs-dev libneon27-dev libarchive-dev libpcre3-dev cmake g++ git
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+
+# Download release
+wget https://github.com/elfmz/far2l/archive/refs/tags/v_2.4.0.tar.gz -O v_2.4.0.tar.gz
+tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
+
+cmake -DUSEWX=no -DNETROCKS=no -DUSEUCD=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" .
+
+cmake --build . --config Release
+
+cp -rf "$dp0/release/far2l-v_2.4.0/install/." "$dp0/release/build/"
+
+cd "$dp0/release/build"
+strip "far2l"
+chmod +x "far2l"
+ldd "far2l"
+"./far2l" --help
