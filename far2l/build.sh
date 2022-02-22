854dfb7839a4a7272b1e596f30145f5cd689c28e
Решетников Александр (Reshetnikov_AS) <hemn.still@gmail.com>
2022-02-22 14:28:49 +0400

    -DUSEUCD

--- far2l/build.sh
+++ far2l/build.sh
@@ -15,7 +15,7 @@ tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
 cp -f "../SafeMMap.cpp" "./far2l/src/base/"
 
-cmake -DUSEWX=no -DNETROCKS=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-static" .
+cmake -DUSEWX=no -DNETROCKS=no -DUSEUCD=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-static" .
 
 cmake --build . --config Release
 
@@ -24,5 +24,4 @@ cp -rf "$dp0/release/far2l-v_2.4.0/install/" "$dp0/release/build/"
 cd "$dp0/release/build"
 strip "far2l"
 chmod +x "far2l"
-ldd "far2l"
 "./far2l" --help
