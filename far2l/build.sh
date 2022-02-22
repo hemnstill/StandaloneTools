09e11b1de1c9b7d501eb48a7ee3473342cbe3892
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-23 01:30:38 +0400

    linux-headers

--- far2l/build.sh
+++ far2l/build.sh
@@ -3,7 +3,7 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake alpine-sdk musl-dev git
+apk add --no-cache gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake alpine-sdk linux-headers git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
 apk add --no-cache libexecinfo-dev
 
