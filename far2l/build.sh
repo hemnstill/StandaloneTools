54b73dc3f6b757d76921375bbc3981cdc63fd813
Решетников Александр (Reshetnikov_AS) <hemn.still@gmail.com>
2022-02-22 12:52:46 +0400

    uchardet-static

--- far2l/build.sh
+++ far2l/build.sh
@@ -5,7 +5,7 @@ set -e
 apk update
 apk add --no-cache alpine-sdk gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake g++ git
 apk add --no-cache pcre2-dev uchardet-dev neon-dev spdlog-dev xerces-c-dev
-apk add --no-cache musl-dev linux-headers libexecinfo-dev libexecinfo-static
+apk add --no-cache musl-dev linux-headers libexecinfo-dev libexecinfo-static uchardet-static
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 
@@ -15,13 +15,13 @@ tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
 cp -f "../SafeMMap.cpp" "./far2l/src/base/"
 
-cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" .
+cmake -DUSEWX=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++ -lstdc++ -luchardet" .
 
 cmake --build . --config Release
 
 cp -rf "$dp0/release/far2l-v_2.4.0/install/" "$dp0/release/build/"
 
 cd "$dp0/release/build"
-strip "far2l"
 chmod +x "far2l"
+ldd "far2l"
 "./far2l" --version
