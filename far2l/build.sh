f24465334eaeda5ef985222c08a6853c590b184e
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-02-25 02:56:09 +0400

    Far2l without plugins (#3)
    - alpine (musl)
    - ubuntu (glibc)

---
+++ far2l/build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache gawk m4 libssh-dev libressl-dev libnfs-dev libarchive-dev cmake alpine-sdk linux-headers musl-dev git
+apk add --no-cache uchardet-dev  neon-dev spdlog-dev xerces-c-dev libexecinfo-dev
+apk add --no-cache uchardet-static libexecinfo-static
+
+echo "::endgroup::"
+
+echo "::group::prepare sources v_2.4.0"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+
+# Download release
+wget https://github.com/elfmz/far2l/archive/refs/tags/v_2.4.0.tar.gz -O v_2.4.0.tar.gz
+tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
+
+cp -f "../SafeMMap.cpp" "./far2l/src/base/"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+without_plugins="\
+-DALIGN=no \
+-DAUTOWRAP=no \
+-DCALC=no \
+-DCOLORER=no \
+-DCOMPARE=no \
+-DDRAWLINE=no \
+-DEDITCASE=no \
+-DEDITORCOMP=no \
+-DFARFTP=no \
+-DFILECASE=no \
+-DINCSRCH=no \
+-DINSIDE=no \
+-DMULTIARC=no \
+-DNETROCKS=no \
+-DSIMPLEINDENT=no \
+-DTMPPANEL=no \
+"
+
+cmake_command=$(printf 'cmake -DUSEWX=no -DUSEUCD=no %s -DCMAKE_EXE_LINKER_FLAGS="%s" -DCMAKE_BUILD_TYPE=Release .' \
+  "$without_plugins" \
+  "-l:libuchardet.a -static-libstdc++ -static-libgcc")
+echo ">> $cmake_command"
+eval "$cmake_command"
+
+cmake --build . --config Release
+
+echo "::endgroup::"
+
+cp -rf "$dp0/release/far2l-v_2.4.0/install/." "$dp0/release/build/"
+
+cd "$dp0/release/build"
+chmod +x "far2l"
+ldd "far2l"
+"./far2l" --help | head -n1
