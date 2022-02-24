1426753f62974318ad0d150c6b41a1ee62a05fa3
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-24 22:23:19 +0400

    -l:libuchardet.a

--- far2l/build.sh
+++ far2l/build.sh
@@ -15,7 +15,7 @@ tar -xf v_2.4.0.tar.gz && cd far2l-v_2.4.0
 
 cp -f "../SafeMMap.cpp" "./far2l/src/base/"
 
-cmake -DUSEWX=no -DNETROCKS=no -DCMAKE_EXE_LINKER_FLAGS="-Wl,-Bstatic -luchardet -lexecinfo -static-libstdc++ -lstdc++ -Wl,-Bdynamic" -DCMAKE_BUILD_TYPE=Release .
+cmake -DUSEWX=no -DNETROCKS=no -DCMAKE_EXE_LINKER_FLAGS="-Wl,-Bstatic -l:libuchardet.a -static-libstdc++ -lstdc++ -Wl,-Bdynamic" -DCMAKE_BUILD_TYPE=Release .
 
 cmake --build . --config Release
 
