2799eb8031483ca13502c3f0dd9981b39e9f2f88
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-25 00:37:24 +0400

    libuchardet.a deb

--- far2l_deb/build.sh
+++ far2l_deb/build.sh
@@ -30,7 +30,9 @@ without_plugins="\
 -DTMPPANEL=no \
 "
 
-cmake_command=$(printf 'cmake -DUSEWX=no %s -DCMAKE_EXE_LINKER_FLAGS="%s" -DCMAKE_BUILD_TYPE=Release .' "$without_plugins" "")
+cmake_command=$(printf 'cmake -DUSEWX=no %s -DCMAKE_EXE_LINKER_FLAGS="%s" -DCMAKE_BUILD_TYPE=Release .' \
+"$without_plugins" \
+"-l:libuchardet.a")
 echo ">> $cmake_command"
 eval "$cmake_command"
 
