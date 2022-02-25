f621679a615c3640ec4e324726468befcd044a00
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-02-26 01:41:54 +0400

    pcre2grep 10.39 release (#6)

--- pcre2grep/build.sh
+++ pcre2grep/build.sh
@@ -2,22 +2,44 @@
 dp0="$(realpath "$(dirname "$0")")"
 set -e
 
+echo "::group::install deps"
+
 apk update
 apk add --no-cache alpine-sdk pcre2-dev
 
-mkdir -p "$dp0/release" && cd "$dp0/release"
+echo "::endgroup::"
 
-# Download release
-wget https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.39/pcre2-10.39.tar.gz -O pcre2-10.39.tar.gz
-tar -xf pcre2-10.39.tar.gz && cd pcre2-10.39
+tool_name="pcre2grep"
+tool_version="10.39"
+echo "::set-output name=tool_name::$tool_name"
+echo "::set-output name=tool_version::$tool_version"
+
+download_url="https://github.com/PhilipHazel/pcre2/releases/download/pcre2-$tool_version/pcre2-$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+wget "$download_url" -O "pcre2-$tool_version.tar.gz"
+tar -xf "pcre2-$tool_version.tar.gz" && cd "pcre2-$tool_version"
+
+echo "::endgroup::"
+
+echo "::group::build"
 
 ./configure LDFLAGS='--static' --disable-shared --enable-jit --enable-pcre2-8 --enable-pcre2-16 --enable-pcre2-32
 
 make -j$(nproc)
 
-cp -f "$dp0/release/pcre2-10.39/pcre2grep" "$dp0/release/"
+echo "::endgroup::"
+
+cp -f "$dp0/release/pcre2-$tool_version/$tool_name" "$dp0/release/"
 
 cd "$dp0/release"
-strip "pcre2grep"
-chmod +x "pcre2grep"
-"./pcre2grep" --version
+strip "$tool_name"
+chmod +x "$tool_name"
+
+{ printf 'SHA-256: %s
+%s
+%s' "$(sha256sum < $tool_name)" "$("./$tool_name" --version)" "$download_url"
+} > body.md
+
+cat body.md
