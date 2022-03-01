8a27e7be35efedbbae59b7bd63f4fcf3cb7349e2
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-03-01 23:06:08 +0400

    download ci.cmd - upload-artifact

--- bsdtar/build_mingw.sh
+++ bsdtar/build_mingw.sh
@@ -14,15 +14,19 @@ mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "libarchive-$tool_version.tar.gz"
 tar -xf "libarchive-$tool_version.tar.gz" && cd "libarchive-$tool_version"
 
+wget "https://github.com/libarchive/libarchive/raw/bsdtar-$tool_version/build/ci/github_actions/ci.cmd" -O "ci.cmd"
+
 echo "::endgroup::"
 
 echo "::group::build"
 
 export BE=mingw-gcc
 
-"$dp0/ci.cmd" deplibs
-"$dp0/ci.cmd" configure
-"$dp0/ci.cmd" build
-"$dp0/ci.cmd" artifact
+"ci.cmd" deplibs
+"ci.cmd" configure
+"ci.cmd" build
+"ci.cmd" artifact
 
 echo "::endgroup::"
+
+cp -f "$dp0/release/libarchive-$tool_version/libarchive.zip" "$dp0/release/"
