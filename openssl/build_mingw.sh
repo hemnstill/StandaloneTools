02754544d25212c97b112cfd70f3bad39a008cd6
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-03-07 00:27:04 +0400

    openssl-

--- openssl/build_mingw.sh
+++ openssl/build_mingw.sh
@@ -14,12 +14,12 @@ tool_version="3.0.1"
 echo "::set-output name=tool_name::$tool_name"
 echo "::set-output name=tool_version::$tool_version"
 
-download_url="https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_$tool_version.tar.gz"
+download_url="https://github.com/openssl/openssl/archive/refs/tags/openssl-$tool_version.tar.gz"
 echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "tool-$tool_version.tar.gz"
-tar -xf "tool-$tool_version.tar.gz" && cd "openssl-OpenSSL_$tool_version"
+tar -xf "tool-$tool_version.tar.gz" && cd "openssl-openssl-$tool_version"
 
 echo "::endgroup::"
 
@@ -31,7 +31,7 @@ make
 echo "::endgroup::"
 
 mkdir "$dp0/release/build" && cd "$dp0/release/build"
-cp -f "$dp0/release/openssl-OpenSSL_$tool_version/apps/$tool_name" "$dp0/release/build/"
+cp -f "$dp0/release/openssl-openssl-$tool_version/apps/$tool_name" "$dp0/release/build/"
 
 { printf 'SHA-256: %s
 %s
