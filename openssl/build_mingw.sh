b065c891280fcd18f0bd56d160d0d7698d7cd7a5
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-03-06 21:34:29 +0400

    release/build

--- openssl/build_mingw.sh
+++ openssl/build_mingw.sh
@@ -25,9 +25,8 @@ nmake /S
 
 echo "::endgroup::"
 
-cp -f "$dp0/release/openssl-OpenSSL_$tool_version/apps/$tool_name" "$dp0/release/"
-
-cd "$dp0/release"
+mkdir "$dp0/release/build" && cd "$dp0/release/build"
+cp -f "$dp0/release/openssl-OpenSSL_$tool_version/apps/$tool_name" "$dp0/release/build/"
 
 { printf 'SHA-256: %s
 %s
@@ -35,3 +34,5 @@ cd "$dp0/release"
 } > _mingw.md
 
 cat _mingw.md
+
+tar -czvf ../_mingw.tar.gz .
