b86aef4306c54824887bd65e59b3d61ec8d23232
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-03-07 23:36:02 +0400

    Openssl 1.1.1m (#13)

--- openssl/build_mingw.sh
+++ openssl/build_mingw.sh
@@ -10,28 +10,28 @@ apk add --no-cache alpine-sdk perl make linux-headers mingw-w64-gcc
 echo "::endgroup::"
 
 tool_name="openssl.exe"
-tool_version="3.0.1"
+tool_version="1_1_1m"
 echo "::set-output name=tool_name::$tool_name"
 echo "::set-output name=tool_version::$tool_version"
 
-download_url="https://github.com/openssl/openssl/archive/refs/tags/openssl-$tool_version.tar.gz"
+download_url="https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_$tool_version.tar.gz"
 echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "tool-$tool_version.tar.gz"
-tar -xf "tool-$tool_version.tar.gz" && cd "openssl-openssl-$tool_version"
+tar -xf "tool-$tool_version.tar.gz" && cd "openssl-OpenSSL_$tool_version"
 
 echo "::endgroup::"
 
 echo "::group::build"
 
-./Configure --banner=Configured mingw64 --cross-compile-prefix=x86_64-w64-mingw32- no-shared
+./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- no-shared
 make
 
 echo "::endgroup::"
 
 mkdir "$dp0/release/build" && cd "$dp0/release/build"
-cp -f "$dp0/release/openssl-openssl-$tool_version/apps/$tool_name" "$dp0/release/build/"
+cp -f "$dp0/release/openssl-OpenSSL_$tool_version/apps/$tool_name" "$dp0/release/build/"
 
 { printf 'SHA-256: %s
 %s
