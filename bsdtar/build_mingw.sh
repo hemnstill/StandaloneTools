a94381739dafd9fd8ff2b7ac3d374b247b1c8dce
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-03-31 21:34:58 +0400

    zstd deplibs

--- bsdtar/build_mingw.sh
+++ bsdtar/build_mingw.sh
@@ -14,7 +14,9 @@ mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "libarchive-$tool_version.tar.gz"
 tar -xf "libarchive-$tool_version.tar.gz" && cd "libarchive-$tool_version"
 
-wget "https://github.com/libarchive/libarchive/raw/v$tool_version/build/ci/github_actions/ci.cmd" -O "$dp0/ci.cmd"
+# wget "https://github.com/libarchive/libarchive/raw/v$tool_version/build/ci/github_actions/ci.cmd" -O "$dp0/ci.cmd"
+
+cp -f "../ci.cmd" "../../"
 
 echo "::endgroup::"
 
@@ -23,8 +25,8 @@ echo "::group::build"
 export BE=mingw-gcc
 
 "$dp0/ci.cmd" deplibs
-"$dp0/ci.cmd" configure
-"$dp0/ci.cmd" build
+# "$dp0/ci.cmd" configure
+# "$dp0/ci.cmd" build
 
 echo "::endgroup::"
 
