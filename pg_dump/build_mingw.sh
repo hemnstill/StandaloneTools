8ddd0f149e172d113f75503eebe3b6af5e6be3c8
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-04-26 00:03:42 +0400

    restore alpine mingw

--- pg_dump/build_mingw.sh
+++ pg_dump/build_mingw.sh
@@ -17,20 +17,26 @@ echo "::set-output name=tool_version::$tool_version"
 download_url="https://github.com/postgres/postgres/archive/refs/tags/$tool_version.tar.gz"
 echo "::group::prepare sources $download_url"
 
-mkdir -p "$dp0/release" && cd "$dp0/release"
+mkdir -p "$dp0/release/build" && cd "$dp0/release"
 wget "$download_url" -O "tool-$tool_version.tar.gz" && tar -xf "tool-$tool_version.tar.gz"
-cd "postgres-$tool_version"
+tool_root_path="$dp0/release/postgres-$tool_version"
+cd "$tool_root_path"
 
 echo "::endgroup::"
 
 echo "::group::build"
 
-mkdir -p "$dp0/release/build"
-cd "$dp0/release/build"
-"$dp0/release/postgres-$tool_version/configure" --without-readline --without-zlib --with-system-tzdata=/usr/share/zoneinfo --host=x86_64-w64-mingw32
+./configure --without-readline --without-zlib --with-system-tzdata=/usr/share/zoneinfo --host=x86_64-w64-mingw32
 make -j$(nproc)
 
 echo "::endgroup::"
+
+cp -rf "$tool_root_path/src/bin/pg_dump/pg_dump.exe" "$dp0/release/build/"
+cp -rf "$tool_root_path/src/bin/pg_dump/pg_dumpall.exe" "$dp0/release/build/"
+cp -rf "$tool_root_path/src/bin/pg_dump/pg_restore.exe" "$dp0/release/build/"
+cp -rf "$tool_root_path/src/interfaces/libpq/libpq.dll" "$dp0/release/build/"
+
+cd "$dp0/release/build"
 
 { printf 'SHA-256: %s
 ' "$(sha256sum $tool_name)"
