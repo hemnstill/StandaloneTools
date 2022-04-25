5d30cf1396755131ce59d7611944b43d0676fbd4
Решетников Александр (Reshetnikov_AS) <hemn.still@gmail.com>
2022-04-25 16:09:10 +0400

    alpine mingw

---
+++ pg_dump/build_mingw.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk linux-headers zlib-dev zlib-static postgresql-dev perl-dev bison flex
+
+echo "::endgroup::"
+
+tool_name="pg_dump"
+tool_version="REL_14_2"
+echo "::set-output name=tool_name::$tool_name"
+echo "::set-output name=tool_version::$tool_version"
+
+download_url="https://github.com/postgres/postgres/archive/refs/tags/$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+wget "$download_url" -O "tool-$tool_version.tar.gz" && tar -xf "tool-$tool_version.tar.gz"
+cd "postgres-$tool_version"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+./configure --without-readline --host=x86_64-w64-mingw32
+make -j$(nproc)
+
+echo "::endgroup::"
+
+mkdir "$dp0/release/build"
+
+cp -rf "$dp0/release/postgres-$tool_version/src/bin/pg_dump/." "$dp0/release/build/"
+
+cd "$dp0/release/build"
+
+strip "$tool_name"
+
+{ printf 'SHA-256: %s
+' "$(sha256sum $tool_name)"
+} > build-mingw.md
+
+cat build-mingw.md
+
+tar -czvf ../build-mingw.tar.gz .
