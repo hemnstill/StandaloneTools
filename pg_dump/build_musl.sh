5756ace415b25572c0b936254fbc5f2b7fb2a89a
Решетников Александр (Reshetnikov_AS) <hemn.still@gmail.com>
2022-04-25 14:43:18 +0400

    postgres-

--- pg_dump/build_musl.sh
+++ pg_dump/build_musl.sh
@@ -19,7 +19,7 @@ echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "tool-$tool_version.tar.gz"
-tar -xf "tool-$tool_version.tar.gz" && cd "tool-$tool_version"
+tar -xf "tool-$tool_version.tar.gz" && cd "postgres-$tool_version"
 
 echo "::endgroup::"
 
