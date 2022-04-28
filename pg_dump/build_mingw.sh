50733c7c8d9b63bfdea70c1c35b749c6140242d8
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-04-28 23:44:14 +0400

    pg_dump, pg_dumpall, pg_restore 14.2 (#20)
    postgresql-client static build

---
+++ pg_dump/build_mingw.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk linux-headers zlib-dev zlib-static postgresql-dev perl-dev bison flex mingw-w64-gcc icu-dev
+
+echo "::endgroup::"
+
+tool_name="pg_dump.exe"
+tool_version="REL_14_2"
+echo "::set-output name=tool_name::$tool_name"
+echo "::set-output name=tool_version::$tool_version"
+
+download_url="https://github.com/postgres/postgres/archive/refs/tags/$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+tool_root_path="$dp0/release/postgres-$tool_version"
+
+mkdir -p "$dp0/release/build" && cd "$dp0/release"
+
+if [[ ! -f "$tool_root_path/configure" ]]; then
+  wget "$download_url" -O "tool-$tool_version.tar.gz" && tar -xf "tool-$tool_version.tar.gz"
+fi
+
+cd "$tool_root_path"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+./configure --without-readline --without-zlib --with-system-tzdata=/usr/share/zoneinfo --host=x86_64-w64-mingw32
+make -j$(nproc)
+
+echo "::endgroup::"
+
+cp "$tool_root_path/src/bin/pg_dump/pg_dump.exe" "$dp0/release/build/"
+cp "$tool_root_path/src/bin/pg_dump/pg_dumpall.exe" "$dp0/release/build/"
+cp "$tool_root_path/src/bin/pg_dump/pg_restore.exe" "$dp0/release/build/"
+cp "$tool_root_path/src/interfaces/libpq/libpq.dll" "$dp0/release/build/"
+
+cd "$dp0/release/build"
+
+{ printf 'SHA-256: %s
+%s
+' "$(sha256sum $tool_name)" "$download_url"
+} > build-mingw.md
+
+cat build-mingw.md
+
+tar -czvf ../build-mingw.tar.gz .
---
+++ pg_dump/build_musl.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk linux-headers zlib-dev zlib-static postgresql-dev perl-dev bison flex libpq-dev
+
+echo "::endgroup::"
+
+tool_name="pg_dump"
+tool_version="REL_14_2"
+echo "::set-output name=tool_name::$tool_name"
+echo "::set-output name=tool_version::$tool_version"
+
+download_url="https://github.com/postgres/postgres/archive/refs/tags/$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+tool_root_path="$dp0/release/postgres-$tool_version"
+
+mkdir -p "$dp0/release/build" && cd "$dp0/release"
+
+if [[ ! -f "$tool_root_path/configure" ]]; then
+  wget "$download_url" -O "tool-$tool_version.tar.gz" && tar -xf "tool-$tool_version.tar.gz"
+fi
+
+cp "$dp0/release/Makefile" "$tool_root_path/src/bin/pg_dump/"
+
+cd "$tool_root_path"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+./configure --without-readline --without-zlib
+
+make -j$(nproc)
+
+echo "::endgroup::"
+
+cp "$tool_root_path/src/bin/pg_dump/pg_dump" "$dp0/release/build/"
+cp "$tool_root_path/src/bin/pg_dump/pg_dumpall" "$dp0/release/build/"
+cp "$tool_root_path/src/bin/pg_dump/pg_restore" "$dp0/release/build/"
+
+cd "$dp0/release/build"
+
+strip "$tool_name"
+strip "pg_dumpall"
+strip "pg_restore"
+
+chmod +x "$tool_name"
+chmod +x "pg_dumpall"
+chmod +x "pg_restore"
+
+{ printf 'SHA-256: %s
+%s
+' "$(sha256sum $tool_name)" "$("./$tool_name" --version)"
+} > build-musl.md
+
+cat build-musl.md
+
+tar -czvf ../build-musl.tar.gz .
