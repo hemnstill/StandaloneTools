6f09dee269c3915c0d372837160063d45bce6897
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-05-21 19:32:16 +0400

    Bsdtar unittests (#25)
    add test-ubuntu
    add shunit2 47be8b23a46a7897e849f1841f0fb704d34d0f6e
    
    `test-*` pipelines need `test-run` → `workflow`

---
+++ bsdtar/test_windows.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+testVersion() {
+  assertEquals "bsdtar 3.6.1 - libarchive 3.6.1 zlib/1.2.12 liblzma/5.2.5 libzstd/1.5.2 " "$(../bin/bsdtar.exe --version)"
+}
+
+# Load and run shUnit2.
+source "../.tests/shunit2/shunit2"
