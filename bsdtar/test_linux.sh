c66b4313b5fdcd9046ec9b8529cd7370b3520f5e
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-21 18:45:58 +0400

    test linux and windows

--- bsdtar/test_linux.sh
+++ bsdtar/test_linux.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-dp0="$(realpath "$(dirname "$0")")" && cd "$dp0" || exit 1
 
 testVersion() {
   assertEquals "bsdtar 3.6.1 - libarchive 3.6.1 zlib/1.2.12 liblzma/5.2.5 libzstd/1.5.2 " "$(../bin/bsdtar --version)"
