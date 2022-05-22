34b721a008fdd74e7af891d624e33dfaf671e41a
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-22 16:50:26 +0400

    test double quotes

--- pcre2grep/test_windows.sh
+++ pcre2grep/test_windows.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+grep="../bin/pcre2grep.exe"
+
 testVersion() {
-  assertEquals "pcre2grep version 10.40 2022-04-14" "$(../bin/pcre2grep.exe --version)"
+  assertEquals "pcre2grep version 10.40 2022-04-14" "$("$grep" --version)"
+}
+
+testDoubleQuotes() {
+   assertEquals "test-win64.zip" "$(echo "test-win64.zip" | "$grep" --only-matching "[^"" ]*win64\.zip")"
 }
 
 # Load and run shUnit2.
