4a5fb84624f7f79889a1a8cdfb78bd9e591238a8
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-22 19:13:29 +0400

    --enable-rebuild-chartables ?

--- pcre2grep/build_mingw.sh
+++ pcre2grep/build_mingw.sh
@@ -25,7 +25,7 @@ echo "::endgroup::"
 
 echo "::group::build"
 
-./configure LDFLAGS='--static' --disable-shared --enable-jit --enable-pcre2-16 --enable-pcre2-32 --host=x86_64-w64-mingw32
+./configure LDFLAGS='--static' --disable-shared --enable-rebuild-chartables --enable-jit --enable-pcre2-16 --enable-pcre2-32 --host=x86_64-w64-mingw32
 
 make -j$(nproc)
 
