4fb9ff775ef647d1cb32489e30fb99640cc91c6b
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-06-04 20:18:16 +0400

    bb

---
+++ busybox/build_mingw.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+tool_name="bsdtar.exe"
+tool_version="1_34_1"
+echo "::set-output name=tool_name::$tool_name"
+echo "::set-output name=tool_version::$tool_version"
+
+download_url="https://github.com/rmyorston/$tool_name-w32/archive/refs/tags/$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+tar -xf "tool-$tool_version.tar.gz" && cd "tool-$tool_version"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+make -j$(nproc) mingw64_defconfig
+make -j$(nproc) AR=x86_64-w64-mingw32-gcc-ar STRIP=strip WINDRES=windres busybox.exe
+
+echo "::endgroup::"
+
+mkdir "$dp0/release/build" && cd "$dp0/release/build"
+
+{ printf 'SHA-256: %s
+%s
+%s
+' "$(sha256sum $tool_name)" "$("./$tool_name" --version)" "$download_url"
+} > build-mingw.md
+
+cat build-mingw.md
+
+tar -czvf ../build-mingw.tar.gz .
