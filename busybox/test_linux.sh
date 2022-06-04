1e8328435019af478c70b28aa2437602270e90b9
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-06-04 23:37:00 +0400

    test

---
+++ busybox/test_linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+testVersion() {
+  assertEquals "tar (busybox) 1.36.0.git" "$(../bin/busybox tar --version | head -1)"
+}
+
+# Load and run shUnit2.
+source "../.tests/shunit2/shunit2"
