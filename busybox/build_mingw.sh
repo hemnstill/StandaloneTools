f7107ed6d1a81d6dd2922af4f0e0da483f45ff61
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-06-04 20:22:52 +0400

    ~

--- busybox/build_mingw.sh
+++ busybox/build_mingw.sh
@@ -2,12 +2,12 @@
 dp0="$(realpath "$(dirname "$0")")"
 set -e
 
-tool_name="bsdtar.exe"
+tool_name="busybox.exe"
 tool_version="1_34_1"
 echo "::set-output name=tool_name::$tool_name"
 echo "::set-output name=tool_version::$tool_version"
 
-download_url="https://github.com/rmyorston/$tool_name-w32/archive/refs/tags/$tool_version.tar.gz"
+download_url="https://github.com/rmyorston/busybox-w32/archive/refs/tags/$tool_version.tar.gz"
 echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
