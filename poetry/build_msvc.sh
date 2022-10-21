a62d889e3b04f9a6766a35f6b55c1b950f4c5c24
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-10-22 00:45:59 +0400

    venv

--- poetry/build_msvc.sh
+++ poetry/build_msvc.sh
@@ -45,7 +45,6 @@ if [[ ! -f "$cpython_bin" ]]; then
   --exclude="tests" \
   --exclude="idle_test" \
   --exclude="site-packages" \
-  --exclude="venv" \
   --exclude="Scripts" \
   --exclude="*.pdb" \
   --exclude="*.whl" \
@@ -65,8 +64,7 @@ fi;
 
 echo "install poetry ..."
 export POETRY_HOME="$dp0/.tmp/poetry"
-# "pipx" install poetry=="$tool_version"
-python3 "$dp0/release/$poetry_install_script" --version "$tool_version"
+"$cpython_bin" "$dp0/release/$poetry_install_script" --version "$tool_version"
 
 echo "prepare build artifacts ..."
 rm -rf "$dp0/release/$self_name" && mkdir -p "$dp0/release/$self_name"
