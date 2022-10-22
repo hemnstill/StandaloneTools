da2f4bdbc564eff8aa16063c490b0b5acdf139f3
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-10-22 11:19:20 +0400

    python3 user site

--- poetry/build_musl.sh
+++ poetry/build_musl.sh
@@ -56,6 +56,8 @@ export POETRY_HOME="$dp0/.tmp/poetry"
 "python3" -m pip install wheel
 "python3" -m pip install "$dp0/release/cryptography-38.0.1-cp36-abi3-musllinux_1_1_x86_64.whl"
 
+python3 -m site --user-site
+
 ls /usr/lib/python3.10/site-packages
 
 "$bsdtar" -xf "$dp0/release/cryptography-38.0.1-cp36-abi3-musllinux_1_1_x86_64.whl" -C "$cpython_site_packages"
