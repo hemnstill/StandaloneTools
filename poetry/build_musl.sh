d97bf347c5c0c5ce645ed2a142266053bad84821
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-10-22 10:08:48 +0400

    python3

--- poetry/build_musl.sh
+++ poetry/build_musl.sh
@@ -50,8 +50,7 @@ fi;
 
 echo "install poetry ..."
 export POETRY_HOME="$dp0/.tmp/poetry"
-"$cpython_bin" -m pip install wheel
-"$cpython_bin" -m pip install poetry=="$tool_version"
+"python3" -m pip install poetry=="$tool_version"
 
 echo "prepare build artifacts ..."
 rm -rf "$dp0/release/$self_name" && mkdir -p "$dp0/release/$self_name"
