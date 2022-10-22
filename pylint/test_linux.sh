e4e8df147a628a365fe3312f48fae3f817b7cae3
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-10-23 01:07:19 +0400

    eol test up

--- pylint/test_linux.sh
+++ pylint/test_linux.sh
@@ -3,7 +3,7 @@
 test_version() {
   assertEquals "pylint 2.15.3
 astroid 2.12.12
-Python 3.10.5 (main, Jun 27 2022, 04:27:12) [Clang 14.0.3 ] " "$(../bin/pylint.sh --version)"
+Python 3.10.5 (main, Jun 27 2022, 04:27:12) [Clang 14.0.3 ]" "$(../bin/pylint.sh --version)"
 }
 
 # Load and run shUnit2.
