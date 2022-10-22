054707c5eb2e565cbef2ba7dc001e183dbe1186a
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-10-22 20:04:30 +0400

    use release poetry-1.2.2

--- pylint/build_msvc.sh
+++ pylint/build_msvc.sh
@@ -2,21 +2,19 @@
 dp0="$(realpath "$(dirname "$0")")"
 set -e
 
-is_windows_os=false && [[ $(uname) == Windows_NT* ]] && is_windows_os=true
-
 tool_name="pylint"
 tool_version="2.15.3"
-poetry_name_version="poetry-1.2.1"
+poetry_name_version="poetry-1.2.2"
 echo "::set-output name=tool_name::$tool_name"
 
 mkdir -p "$dp0/release/$tool_name-$tool_version" && cd "$dp0/release"
 
 echo "download poetry install script ..."
-poetry_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/poetry-1.2.1-beta/$poetry_name_version.sh.bat"
-poetry_download_bin="$dp0/release/$poetry_name_version.sh.bat"
-[[ ! -f "$poetry_download_bin" ]] && wget "$poetry_bin_download_url" -O "$poetry_download_bin"
+poetry_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/$poetry_name_version/build-msvc.tar.gz"
+poetry_download_zip="$dp0/release/$poetry_name_version.tar.gz"
+[[ ! -f "$poetry_download_zip" ]] && wget "$poetry_bin_download_url" -O "$poetry_download_zip"
 
-poetry_bin="$dp0/release/$poetry_name_version/poetry.bat"
-[[ ! -f "$poetry_bin" ]] && "$poetry_download_bin"
+poetry_bin="$dp0/release/poetry.bat"
+[[ ! -f "$poetry_bin" ]] && tar -xf "$poetry_download_zip"
 
-$poetry_bin install
+"$poetry_bin" install
