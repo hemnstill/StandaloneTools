58090bf7ed66b8d33e731688ee125d65fb94407a
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-11-04 19:20:15 +0400

    pg_dump 15.0 + psql (#33)

--- pg_dump/test_linux.sh
+++ pg_dump/test_linux.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 test_pg_dump_Version() {
-  assertEquals "pg_dump (PostgreSQL) 14.4" "$(../bin/pg_dump --version)"
+  assertEquals "pg_dump (PostgreSQL) 15.0" "$(../bin/pg_dump --version)"
 }
 
 test_pg_dump_all_Version() {
-  assertEquals "pg_dumpall (PostgreSQL) 14.4" "$(../bin/pg_dumpall --version)"
+  assertEquals "pg_dumpall (PostgreSQL) 15.0" "$(../bin/pg_dumpall --version)"
 }
 
 test_pg_restore_Version() {
-  assertEquals "pg_restore (PostgreSQL) 14.4" "$(../bin/pg_restore --version)"
+  assertEquals "pg_restore (PostgreSQL) 15.0" "$(../bin/pg_restore --version)"
+}
+
+test_psql_Version() {
+  assertEquals "psql (PostgreSQL) 15.0" "$(../bin/psql --version)"
 }
 
 # Load and run shUnit2.
--- pg_dump/test_windows.sh
+++ pg_dump/test_windows.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 test_pg_dump_Version() {
-  assertEquals "pg_dump (PostgreSQL) 14.4" "$(../bin/pg_dump.exe --version)"
+  assertEquals "pg_dump (PostgreSQL) 15.0" "$(../bin/pg_dump.exe --version)"
 }
 
 test_pg_dump_all_Version() {
-  assertEquals "pg_dumpall (PostgreSQL) 14.4" "$(../bin/pg_dumpall.exe --version)"
+  assertEquals "pg_dumpall (PostgreSQL) 15.0" "$(../bin/pg_dumpall.exe --version)"
 }
 
 test_pg_restore_Version() {
-  assertEquals "pg_restore (PostgreSQL) 14.4" "$(../bin/pg_restore.exe --version)"
+  assertEquals "pg_restore (PostgreSQL) 15.0" "$(../bin/pg_restore.exe --version)"
+}
+
+test_psql_Version() {
+  assertEquals "psql (PostgreSQL) 15.0" "$(../bin/psql.exe --version)"
 }
 
 # Load and run shUnit2.
