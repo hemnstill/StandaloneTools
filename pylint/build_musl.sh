9437580d3b38960192713e0337912ece6495971e
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-12-21 22:28:33 +0400

    lazy-object-proxy

--- pylint/build_musl.sh
+++ pylint/build_musl.sh
@@ -26,6 +26,7 @@ bsdtar="$dp0/release/bsdtar"
 cpython_bin="$release_version_dirpath/Scripts/bin/python3"
 [[ ! -f "$cpython_bin" ]] && tar -xf "$poetry_download_zip" -C "$release_version_dirpath"
 
+"$cpython_bin" -m pip install "lazy-object-proxy==1.8.0" --force-reinstall --no-binary :all:
 "$cpython_bin" -m pip install "$tool_name==$tool_version"
 
 cp -f "$dp0/release/pylint.sh" "$release_version_dirpath/"
