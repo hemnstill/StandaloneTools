b9d7c92b570647e329cffa6016302d2072492e74
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-12-21 22:38:53 +0400

    "$cpython_bin" -m pip install "astroid==2.12.12" v2

--- pylint/build_musl.sh
+++ pylint/build_musl.sh
@@ -26,7 +26,7 @@ bsdtar="$dp0/release/bsdtar"
 cpython_bin="$release_version_dirpath/Scripts/bin/python3"
 [[ ! -f "$cpython_bin" ]] && tar -xf "$poetry_download_zip" -C "$release_version_dirpath"
 
-"$cpython_bin" -m pip install "astroid==2.12.12" --force-reinstall --no-binary :all:
+"$cpython_bin" -m pip install "astroid==2.12.12"
 "$cpython_bin" -m pip install "$tool_name==$tool_version"
 
 cp -f "$dp0/release/pylint.sh" "$release_version_dirpath/"
