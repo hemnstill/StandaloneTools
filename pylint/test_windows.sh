7297b3c0b344eaa0cc177d856c7593a198c7eab4
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-12-21 22:58:37 +0400

    test_windows.sh

--- pylint/test_windows.sh
+++ pylint/test_windows.sh
@@ -3,7 +3,7 @@
 test_version() {
   assertEquals "pylint 2.15.9
 astroid 2.12.13
-Python 3.10.9 (main, Dec 21 2022, 04:01:57) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.bat --version | dos2unix )"
+Python 3.10.9 (main, Dec 21 2022, 04:16:31) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.bat --version | dos2unix )"
 }
 
 # Load and run shUnit2.
