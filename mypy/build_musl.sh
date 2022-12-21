23949ef1db40dc62cc5f962eccd0d84ddc719dfb
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-12-21 21:54:47 +0400

    update README.md

--- mypy/build_musl.sh
+++ mypy/build_musl.sh
@@ -11,20 +11,21 @@ release_version_dirpath="$dp0/release/$tool_name-$tool_version"
 mkdir -p "$release_version_dirpath" && cd "$dp0/release"
 
 echo "download poetry install script ..."
-poetry_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/$self_name/build-musl.tar.gz"
-poetry_download_zip="$dp0/release/$self_name.tar.gz"
-[[ ! -f "$poetry_download_zip" ]] && wget "$poetry_bin_download_url" -O "$poetry_download_zip"
+python_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/$self_name/build-musl.tar.gz"
+python_download_zip="$dp0/release/$self_name.tar.gz"
+[[ ! -f "$python_download_zip" ]] && wget "$python_bin_download_url" -O "$python_download_zip"
 
 echo "download bsdtar ..."
-bsdtar_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/bsdtar-3.6.1/build-musl.tar.gz"
-bsdtar_tar_gz="bsdtar-3.6.1_build-musl.tar.gz"
+bsdtar_version=3.6.2
+bsdtar_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/bsdtar-$bsdtar_version/build-musl.tar.gz"
+bsdtar_tar_gz="bsdtar-$bsdtar_version-build-musl.tar.gz"
 [[ ! -f "$bsdtar_tar_gz" ]] && wget "$bsdtar_download_url" -O "$bsdtar_tar_gz"
 tar -xf "$bsdtar_tar_gz"
 
 bsdtar="$dp0/release/bsdtar"
 
 cpython_bin="$release_version_dirpath/Scripts/bin/python3"
-[[ ! -f "$cpython_bin" ]] && tar -xf "$poetry_download_zip" -C "$release_version_dirpath"
+[[ ! -f "$cpython_bin" ]] && tar -xf "$python_download_zip" -C "$release_version_dirpath"
 
 "$cpython_bin" -m pip install "$tool_name==$tool_version"
 
