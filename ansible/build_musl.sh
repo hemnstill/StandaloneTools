98f5e35c7c878904313374074acdbe577d4d5676
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 15:38:38 +0400

    --force-overwrite

--- ansible/build_musl.sh
+++ ansible/build_musl.sh
@@ -7,7 +7,7 @@ apk add --no-cache alpine-sdk python3-dev
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk
-apk add glibc-2.35-r0.apk
+apk add --force-overwrite glibc-2.35-r0.apk
 
 tool_name="ansible"
 tool_version="7.1.0"
