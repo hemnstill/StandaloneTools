5f363b48e3f5df34f6bdfc799d0fe9fb30623e35
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 18:48:49 +0400

    python 3.11

--- ansible/test_linux.sh
+++ ansible/test_linux.sh
@@ -27,10 +27,10 @@ test_version() {
   assertEquals "ansible [core 2.14.1]
   config file = None
   configured module search path = ['/github/home/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
-  ansible python module location = /__w/StandaloneTools/StandaloneTools/bin/python/install/lib/python3.10/site-packages/ansible
+  ansible python module location = /__w/StandaloneTools/StandaloneTools/bin/Scripts/lib/python3.11/site-packages/ansible
   ansible collection location = /github/home/.ansible/collections:/usr/share/ansible/collections
   executable location = /__w/StandaloneTools/StandaloneTools/bin/__main__ansible.py
-  python version = 3.10.9 (main, Dec 21 2022, 04:02:04) [Clang 14.0.3 ] (/__w/StandaloneTools/StandaloneTools/bin/python/install/bin/python3)
+  python version = 3.11.1 (main, Jan 16 2023, 22:40:32) [Clang 15.0.7 ] (/__w/StandaloneTools/StandaloneTools/bin/Scripts/bin/python3)
   jinja version = 3.1.2
   libyaml = True" "$(../bin/ansible.sh --version)"
 }
