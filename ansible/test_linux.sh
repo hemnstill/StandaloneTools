5877d6d32c7dc1260b19bdb9f0f7461fefb7d668
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 17:09:04 +0400

    fix

--- ansible/test_linux.sh
+++ ansible/test_linux.sh
@@ -2,6 +2,9 @@
 
 is_alpine_os=false && [[ -f "/etc/alpine-release" ]] && is_alpine_os=true
 if [[ "$is_alpine_os" == true ]]; then
+  apk update
+  apk add --no-cache gcompat
+
   { printf '%s' "-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApZ2u1KJKUu/fW4A25y9m
 y70AGEa/J3Wi5ibNVGNn1gT1r0VfgeWd0pUybS4UmcHdiNzxJPgoWQhV2SSW1JYu
@@ -12,10 +15,10 @@ Zvo9GI2e2MaZyo9/lvb+LbLEJZKEQckqRj4P26gmASrZEPStwc+yqy1ShHLA0j6m
 1QIDAQAB
 -----END PUBLIC KEY-----"
 } > "/etc/apk/keys/sgerrand.rsa.pub"
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk
-wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-i18n-2.35-r0.apk
-apk add --force-overwrite gcompat glibc-2.35-r0.apk glibc-i18n-2.35-r0.apk
-/usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
+  wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk
+  wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-i18n-2.35-r0.apk
+  apk add --force-overwrite glibc-2.35-r0.apk glibc-i18n-2.35-r0.apk
+  /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
 fi
 
 test_version() {
