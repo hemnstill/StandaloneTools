35de711519408d3a37ff94177da59b01bb166b43
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 16:45:25 +0400

    tests

--- ansible/test_linux.sh
+++ ansible/test_linux.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 test_version() {
-  assertEquals "Poetry (version 1.3.1)" "$(../bin/ansible.sh --version)"
+  assertEquals "ansible [core 2.14.1]
+  config file = None
+  configured module search path = ['/github/home/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
+  ansible python module location = /__w/StandaloneTools/StandaloneTools/bin/python/install/lib/python3.10/site-packages/ansible
+  ansible collection location = /github/home/.ansible/collections:/usr/share/ansible/collections
+  executable location = /__w/StandaloneTools/StandaloneTools/bin/__main__ansible.py
+  python version = 3.10.9 (main, Dec 21 2022, 04:02:04) [Clang 14.0.3 ] (/__w/StandaloneTools/StandaloneTools/bin/python/install/bin/python3)
+  jinja version = 3.1.2
+  libyaml = True" "$(../bin/ansible.sh --version)"
 }
 
 # Load and run shUnit2.
