0ae509ce8acc849876684a285fb83848d3b1d5d3
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 11:29:39 +0400

    use poetry poetry-1.3.1

--- ansible/build_musl.sh
+++ ansible/build_musl.sh
@@ -7,7 +7,7 @@ apk add --no-cache alpine-sdk python3-dev
 
 tool_name="ansible"
 tool_version="7.1.0"
-python_self_name="python-3.10.9"
+python_self_name="poetry-1.3.1"
 self_name="$tool_name-$tool_version"
 release_version_dirpath="$dp0/release/$self_name"
 echo "::set-output name=tool_name::$tool_name"
