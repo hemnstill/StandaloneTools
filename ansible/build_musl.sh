0da08bdcc1cbe6df6a23cdcf3997add66033b513
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 15:44:22 +0400

    gcompat

--- ansible/build_musl.sh
+++ ansible/build_musl.sh
@@ -3,11 +3,12 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk python3-dev
+apk add --no-cache alpine-sdk python3-dev gcompat
 
 wget -q -O /etc/apk/keys/sgerrand.rsa.pub https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub
 wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk
 apk add --force-overwrite glibc-2.35-r0.apk
+
 
 tool_name="ansible"
 tool_version="7.1.0"
