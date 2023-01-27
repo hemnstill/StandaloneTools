882d95a86906092030663934fd837ee286e2de42
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-27 18:29:54 +0400

    glibc

--- python/build_gnu.sh
+++ python/build_gnu.sh
@@ -3,7 +3,20 @@ dp0="$(realpath "$(dirname "$0")")"
 set -e
 
 apk update
-apk add --no-cache alpine-sdk python3-dev
+apk add --no-cache alpine-sdk python3-dev gcompat
+
+{ printf '%s' "-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApZ2u1KJKUu/fW4A25y9m
+y70AGEa/J3Wi5ibNVGNn1gT1r0VfgeWd0pUybS4UmcHdiNzxJPgoWQhV2SSW1JYu
+tOqKZF5QSN6X937PTUpNBjUvLtTQ1ve1fp39uf/lEXPpFpOPL88LKnDBgbh7wkCp
+m2KzLVGChf83MS0ShL6G9EQIAUxLm99VpgRjwqTQ/KfzGtpke1wqws4au0Ab4qPY
+KXvMLSPLUp7cfulWvhmZSegr5AdhNw5KNizPqCJT8ZrGvgHypXyiFvvAH5YRtSsc
+Zvo9GI2e2MaZyo9/lvb+LbLEJZKEQckqRj4P26gmASrZEPStwc+yqy1ShHLA0j6m
+1QIDAQAB
+-----END PUBLIC KEY-----"
+} > "/etc/apk/keys/sgerrand.rsa.pub"
+wget https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk
+apk add --force-overwrite glibc-2.35-r0.apk
 
 tool_name="python"
 tool_version="3.11.1"
