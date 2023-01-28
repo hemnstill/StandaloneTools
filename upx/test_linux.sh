8414c3d3408edba3591c9e988cb3641b0969e3f8
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-28 10:50:12 +0400

    rockylinux v8 need xarfs

--- upx/test_linux.sh
+++ upx/test_linux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 is_fedora_os=false && [[ -f "/etc/fedora-release" ]] && is_fedora_os=true
+is_rockylinux_os=false && [[ -f "/etc/rockylinux-release" ]] && is_rockylinux_os=true
+
 if [[ "is_fedora_os" == true ]]; then
+  yum -y install findutils
+fi
+
+if [[ "is_rockylinux_os" == true ]]; then
   yum -y install findutils
 fi
 
