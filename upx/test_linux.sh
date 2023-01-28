be8b3c6ac084272ce907bd414a26d60a946c8bc1
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-28 10:46:34 +0400

    try create archive

--- upx/test_linux.sh
+++ upx/test_linux.sh
@@ -17,5 +17,9 @@ testPackedBsdtarVersion() {
   assertEquals "bsdtar 3.6.2 - libarchive 3.6.2 zlib/1.2.12 liblzma/5.2.5 libzstd/1.5.2 " "$(../bin/bsdtar --version | head -1)"
 }
 
+testPackedBsdtarCreateArchive() {
+  assertEquals "a bsdtar" "$(../bin/bsdtar -cvf bsdtar.tar.gz ../bin/bsdtar)"
+}
+
 # Load and run shUnit2.
 source "../.tests/shunit2/shunit2"
