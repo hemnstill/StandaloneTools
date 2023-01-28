409b7405d9e4ebed4640a7982c959d9d8da084a0
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-28 11:30:01 +0400

    cat version

--- upx/test_linux.sh
+++ upx/test_linux.sh
@@ -4,10 +4,12 @@ is_fedora_os=false && [[ -f "/etc/fedora-release" ]] && is_fedora_os=true
 is_rockylinux_os=false && [[ -f "/etc/rocky-release" ]] && is_rockylinux_os=true
 
 if [[ "$is_fedora_os" == true ]]; then
+  cat "/etc/fedora-release"
   yum -y install findutils
 fi
 
 if [[ "$is_rockylinux_os" == true ]]; then
+  cat "/etc/rocky-release"
   yum -y install findutils
 fi
 
