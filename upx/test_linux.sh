2929a0a3afc056077dee95ebbacaf6b3a7ad25f1
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-01-28 10:20:48 +0400

    test packed version

--- upx/test_linux.sh
+++ upx/test_linux.sh
@@ -4,5 +4,9 @@ testVersion() {
   assertEquals "upx 3.96" "$(../bin/upx --version | head -1)"
 }
 
+testPackedVersion() {
+  assertEquals "upx 3.96" "$(../bin/upx_packed --version | head -1)"
+}
+
 # Load and run shUnit2.
 source "../.tests/shunit2/shunit2"
