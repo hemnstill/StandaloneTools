f57f306f8f28a025c5a21ccc5957e071144435df
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-03-02 09:08:35 +0400

    libpython3.11.so.1.0

--- python/build_gnu.sh
+++ python/build_gnu.sh
@@ -41,7 +41,7 @@ tar -xf "$bsdtar_tar_gz"
 
 bsdtar="$dp0/release/bsdtar"
 cpython_bin="$dp0/.tmp/python/install/bin/python3"
-cpython_dll="$dp0/.tmp/python/install/lib/libpython3.11m.so.1.0"
+cpython_dll="$dp0/.tmp/python/install/lib/libpython3.11.so.1.0"
 if [[ ! -f "$cpython_bin" ]]; then
   echo extract "$cpython_zip" to "$cpython_bin" ...
   rm -rf "$dp0/.tmp/"* && mkdir -p "$dp0/.tmp" && cd "$dp0/.tmp" || exit 1
