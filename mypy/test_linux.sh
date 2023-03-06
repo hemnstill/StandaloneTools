83797b44ea24cfc2c9274f9a47f6e7e64a9a1693
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-03-06 20:59:28 +0400

    tests up

--- mypy/test_linux.sh
+++ mypy/test_linux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+is_alpine_os=false && [[ -f "/etc/alpine-release" ]] && is_alpine_os=true
+
 test_version() {
-  assertEquals "mypy 1.0.1 (compiled: no)" "$(../bin/mypy.sh --version)"
+  if [[ "$is_alpine_os" == true ]]; then
+    assertEquals "mypy 1.0.1 (compiled: no)" "$(../bin/mypy.sh --version)"
+  else
+    assertEquals "mypy 1.0.1 (compiled: yes)" "$(../bin/mypy.sh --version)"
+  fi
 }
 
 # Load and run shUnit2.
