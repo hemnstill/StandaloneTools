54f117af5f62e5a845638591e9ec8192cc8f3cb4
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-03-06 20:36:00 +0400

    pylint 2.16.4 python 3.11.1 (#49)
    musl -> gnu
    
    ```
       File "/lib/python3.10/site-packages/dill/_dill.py", line 1854, in <module>
         _PyCapsule_New = ctypes.pythonapi.PyCapsule_New
     AttributeError: 'NoneType' object has no attribute 'PyCapsule_New'
    ```

--- pylint/build_gnu.sh
+++ pylint/build_gnu.sh
@@ -2,16 +2,20 @@
 dp0="$(realpath "$(dirname "$0")")"
 set -e
 
+apt update
+apt install -y wget
+
 tool_name="pylint"
-tool_version="2.15.9"
-self_name="python-3.10.9"
+tool_version="2.16.4"
+self_name="python-3.11.1"
+self_toolset_name="build-gnu"
 echo "::set-output name=tool_name::$tool_name"
 
 release_version_dirpath="$dp0/release/$tool_name-$tool_version"
 mkdir -p "$release_version_dirpath" && cd "$dp0/release"
 
 echo "download python install script ..."
-python_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/$self_name/build-musl.tar.gz"
+python_bin_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/$self_name/$self_toolset_name.tar.gz"
 python_download_zip="$dp0/release/$self_name.tar.gz"
 [[ ! -f "$python_download_zip" ]] && wget "$python_bin_download_url" -O "$python_download_zip"
 
@@ -27,11 +31,6 @@ bsdtar="$dp0/release/bsdtar"
 cpython_bin="$release_version_dirpath/Scripts/bin/python3"
 [[ ! -f "$cpython_bin" ]] && tar -xf "$python_download_zip" -C "$release_version_dirpath"
 
-#   File "/lib/python3.10/site-packages/dill/_dill.py", line 1854, in <module>
-#     _PyCapsule_New = ctypes.pythonapi.PyCapsule_New
-# AttributeError: 'NoneType' object has no attribute 'PyCapsule_New'
-"$cpython_bin" -m pip install "dill==0.3.5.1" --no-binary :all:
-
 "$cpython_bin" -m pip install "$tool_name==$tool_version"
 
 cp -f "$dp0/release/pylint.sh" "$release_version_dirpath/"
@@ -40,14 +39,15 @@ cp -f "$dp0/release/__main__pylint.py" "$release_version_dirpath/"
 echo "creating archive ..."
 
 cd "$release_version_dirpath"
-{ printf '%s
-' "$(./"$tool_name.sh" --version)"
-} > build-musl.md
+{ printf '### %s
+%s
+' "$self_toolset_name.tar.gz" "$(./"$tool_name.sh" --version)"
+} > $self_toolset_name.md
 
-cat build-musl.md
+cat $self_toolset_name.md
 
 "$bsdtar" \
   --exclude="__pycache__" \
   --exclude="Scripts/Scripts" \
   --exclude="*.whl" \
-  -czvf ../build-musl.tar.gz .
+  -czvf ../$self_toolset_name.tar.gz .
--- pylint/test_linux.sh
+++ pylint/test_linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+../.tools/install_alpine_glibc.sh
+
 test_version() {
-  assertEquals "pylint 2.15.9
-astroid 2.12.13
-Python 3.10.9 (main, Dec 21 2022, 04:01:57) [Clang 14.0.3 ]" "$(../bin/pylint.sh --version)"
+  assertEquals "pylint 2.16.4
+astroid 2.15.0
+Python 3.11.1 (main, Jan 16 2023, 22:40:32) [Clang 15.0.7 ]" "$(../bin/pylint.sh --version)"
 }
 
 # Load and run shUnit2.
