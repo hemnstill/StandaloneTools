92ecc914f56c317e0cb9c37b0aa3424fa5235990
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-03-06 19:38:22 +0400

    tests up #!

--- pylint/test_linux.sh
+++ pylint/test_linux.sh
@@ -2,8 +2,8 @@
 
 test_version() {
   assertEquals "pylint 2.16.4
-astroid 2.12.13
-Python 3.10.9 (main, Dec 21 2022, 04:01:57) [Clang 14.0.3 ]" "$(../bin/pylint.sh --version)"
+astroid 2.15.0
+Python 3.11.1 (main, Jan 16 2023, 20:56:10) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.sh --version)"
 }
 
 # Load and run shUnit2.
--- pylint/test_windows.sh
+++ pylint/test_windows.sh
@@ -2,8 +2,8 @@
 
 test_version() {
   assertEquals "pylint 2.16.4
-astroid 2.12.13
-Python 3.10.9 (main, Dec 21 2022, 04:16:31) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.bat --version | dos2unix )"
+astroid 2.15.0
+Python 3.11.1 (main, Jan 16 2023, 20:56:10) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.bat --version | dos2unix )"
 }
 
 # Load and run shUnit2.
