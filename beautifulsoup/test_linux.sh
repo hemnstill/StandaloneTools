ecc6ceb404c2a53ebe2bcf44788c6a7797bfbce3
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-03-10 17:55:26 +0400

    lxml version

--- beautifulsoup/test_linux.sh
+++ beautifulsoup/test_linux.sh
@@ -4,7 +4,10 @@
 
 test_version() {
   assertEquals "4.11.2" "$(../bin/Scripts/bin/python3 -c "import bs4; print(bs4.__version__)")"
+}
 
+test_lxml_version() {
+  assertEquals "4.9.2" "$(../bin/Scripts/bin/python3 -c "import lxml; print(lxml.__version__)")"
 }
 
 # Load and run shUnit2.
--- beautifulsoup/test_windows.sh
+++ beautifulsoup/test_windows.sh
@@ -4,5 +4,9 @@ test_version() {
   assertEquals "4.11.2" "$(../bin/Scripts/python.exe -c "import bs4; print(bs4.__version__)")"
 }
 
+test_lxml_version() {
+  assertEquals "4.9.2" "$(../bin/Scripts/python.exe -c "import lxml; print(lxml.__version__)")"
+}
+
 # Load and run shUnit2.
 source "../.tests/shunit2/shunit2"
