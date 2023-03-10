97a99feed10f478be1169c39c6414b83d90de4c0
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-03-10 18:03:20 +0400

    download_bsdtar.sh

---
+++ .tools/download_bsdtar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+is_windows_os=false && [[ $(uname) == Windows_NT* ]] && is_windows_os=true
+
+echo "download bsdtar ..."
+bsdtar_version=3.6.2
+bsdtar_toolset_name="build-musl" && [[ "$is_windows_os" == true ]] && bsdtar_toolset_name="build-mingw"
+
+bsdtar_download_url="https://github.com/hemnstill/StandaloneTools/releases/download/bsdtar-$bsdtar_version/$bsdtar_toolset_name.tar.gz"
+bsdtar_tar_gz="bsdtar-$bsdtar_version-$bsdtar_toolset_name.tar.gz"
+[[ ! -f "$bsdtar_tar_gz" ]] && wget "$bsdtar_download_url" -O "$bsdtar_tar_gz"
+tar -xf "$bsdtar_tar_gz"
