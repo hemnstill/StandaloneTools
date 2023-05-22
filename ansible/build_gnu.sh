e6d3ed27799506f89b18bc3907406fbf9ee6d344
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-05-22 10:16:52 +0400

    ansible-test entrypoint (#60)
    remove `.sh` entrypoints

--- ansible/build_gnu.sh
+++ ansible/build_gnu.sh
@@ -11,8 +11,7 @@ export LC_ALL=en_US.UTF-8
 
 tool_name="ansible"
 tool_version="7.5.0"
-tool_core_version="2.14.5"
-tool_lint_version="6.16.0"
+tool_lint_version="6.16.2"
 python_self_name="python-3.11.3"
 self_name="$tool_name-$tool_version"
 release_version_dirpath="$dp0/release/$self_name"
@@ -43,36 +42,34 @@ echo "install ansbile ..."
 "python3" -m ensurepip
 "python3" -m pip install --target="$cpython_lib_path" cffi
 
-"$cpython_bin" -m pip install "ansible-core==$tool_core_version"
 "$cpython_bin" -m pip install "$tool_name==$tool_version"
 "$cpython_bin" -m pip install "ansible-lint==$tool_lint_version"
 
 echo "prepare build artifacts ..."
 
-cp -f "$dp0/release/ansible.sh" "$release_version_dirpath/"
 cp -f "$dp0/release/__main__ansible.py" "$release_version_dirpath/"
 cp -f "$dp0/release/_ansible" "$release_version_dirpath/Scripts/bin/ansible"
 
-cp -f "$dp0/release/ansible-config.sh" "$release_version_dirpath/"
 cp -f "$dp0/release/__main__ansible-config.py" "$release_version_dirpath/"
-cp -f "$dp0/release/_ansible-config" "$release_version_dirpath/Scripts/bin/ansible-config"
+cp -f "$dp0/release/ansible-config" "$release_version_dirpath/Scripts/bin/"
 
-cp -f "$dp0/release/ansible-playbook.sh" "$release_version_dirpath/"
 cp -f "$dp0/release/__main__ansible-playbook.py" "$release_version_dirpath/"
-cp -f "$dp0/release/_ansible-playbook" "$release_version_dirpath/Scripts/bin/ansible-playbook"
+cp -f "$dp0/release/ansible-playbook" "$release_version_dirpath/Scripts/bin/"
 
-cp -f "$dp0/release/ansible-galaxy.sh" "$release_version_dirpath/"
 cp -f "$dp0/release/__main__ansible-galaxy.py" "$release_version_dirpath/"
-cp -f "$dp0/release/_ansible-galaxy" "$release_version_dirpath/Scripts/bin/ansible-galaxy"
+cp -f "$dp0/release/ansible-galaxy" "$release_version_dirpath/Scripts/bin/"
 
-cp -f "$dp0/release/ansible-lint.sh" "$release_version_dirpath/"
 cp -f "$dp0/release/__main__ansible-lint.py" "$release_version_dirpath/"
+cp -f "$dp0/release/ansible-lint" "$release_version_dirpath/Scripts/bin/"
+
+cp -f "$dp0/release/__main__ansible-test.py" "$release_version_dirpath/"
+cp -f "$dp0/release/ansible-test" "$release_version_dirpath/Scripts/bin/"
 
 echo "creating archive ..."
 
 cd "$release_version_dirpath"
 
-./"$tool_name.sh" --version
+"./Scripts/bin/$tool_name" --version
 
 { printf '### build-gnu.tar.gz
 %s
@@ -85,11 +82,14 @@ cd "$release_version_dirpath"
 
 %s
 
-' "$(./"$tool_name.sh" --version)" \
-  "$(./"ansible-config.sh" --version | head -1)" \
-  "$(./"ansible-playbook.sh" --version | head -1)" \
-  "$(./"ansible-galaxy.sh" --version | head -1)" \
-  "$(./"ansible-lint.sh" --version | head -1)"
+%s
+
+' "$("./Scripts/bin/$tool_name" --version)" \
+  "$("./Scripts/bin/ansible-config" --version | head -1)" \
+  "$("./Scripts/bin/ansible-playbook" --version | head -1)" \
+  "$("./Scripts/bin/ansible-galaxy" --version | head -1)" \
+  "$("./Scripts/bin/ansible-lint" --version | head -1)" \
+  "$("./Scripts/bin/ansible-test" --version | head -1)"
 } > build-gnu.md
 
 cat build-gnu.md
@@ -97,5 +97,6 @@ cat build-gnu.md
 "$bsdtar" \
   --exclude="__pycache__" \
   --exclude="Scripts/Scripts" \
+  --exclude="Scripts/lib/python3.10" \
   --exclude="*.whl" \
   -czvf ../build-gnu.tar.gz .
--- ansible/test_linux.sh
+++ ansible/test_linux.sh
@@ -23,10 +23,10 @@ test_version() {
   configured module search path = ['/github/home/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
   ansible python module location = /__w/StandaloneTools/StandaloneTools/bin/Scripts/lib/python3.11/site-packages/ansible
   ansible collection location = /github/home/.ansible/collections:/usr/share/ansible/collections
-  executable location = /__w/StandaloneTools/StandaloneTools/bin/__main__ansible.py
+  executable location = /__w/StandaloneTools/StandaloneTools/bin/Scripts/bin/../../__main__ansible.py
   python version = 3.11.3 (main, May  7 2023, 19:33:53) [Clang 16.0.3 ] (/__w/StandaloneTools/StandaloneTools/bin/Scripts/bin/python3)
   jinja version = 3.1.2
-  libyaml = True" "$(../bin/ansible.sh --version)"
+  libyaml = True" "$(../bin/Scripts/bin/ansible --version)"
 }
 
 test_bin_version() {
@@ -42,11 +42,15 @@ test_ansible_playbook_version() {
 }
 
 test_ansible_galaxy_version() {
-  assertEquals "ansible-galaxy [core 2.14.5]" "$(../bin/ansible-galaxy.sh --version | head -1)"
+  assertEquals "ansible-galaxy [core 2.14.5]" "$(../bin/Scripts/bin/ansible-galaxy --version | head -1)"
 }
 
 test_ansible_lint_version() {
-  assertEquals "ansible-lint 6.16.0 using ansible 2.14.5" "$(../bin/ansible-lint.sh --version | head -1)"
+  assertEquals "ansible-lint 6.16.2 using ansible-core:2.14.5 ruamel-yaml:0.17.26 ruamel-yaml-clib:0.2.7" "$(../bin/Scripts/bin/ansible-lint --version | head -1)"
+}
+
+test_ansible_test_version() {
+  assertEquals "ansible-test version 2.14.5" "$(../bin/Scripts/bin/ansible-test --version | head -1)"
 }
 
 # Load and run shUnit2.
