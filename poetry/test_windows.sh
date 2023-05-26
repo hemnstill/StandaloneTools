e1116928835b65b2e055bbad60e7b28be4e86819
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-05-26 09:44:20 +0400

    setx

--- poetry/test_windows.sh
+++ poetry/test_windows.sh
@@ -1,7 +1,55 @@
 #!/bin/bash
 
+pyproject_content='
+[tool.poetry]
+name = "StandaloneTools"
+version = "1.0.0"
+description = ""
+authors = []
+readme = "README.md"
+
+[tool.poetry.dependencies]
+python = "3.11.3"
+requests = "2.28.2"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
+'
+
+poetry_install_stdout='Updating dependencies
+Resolving dependencies...
+
+Package operations: 5 installs, 0 updates, 0 removals
+
+  • Installing certifi (2023.5.7)
+  • Installing charset-normalizer (3.1.0)
+  • Installing idna (3.4)
+  • Installing urllib3 (1.26.16)
+  • Installing requests (2.28.2)
+
+Writing lock file'
+
 test_version() {
   assertEquals "Poetry (version 1.5.0)" "$(../bin/poetry.bat --version)"
+}
+
+test_install_from_path() {
+  { printf '%s' "$pyproject_content"
+  } > pyproject.toml
+
+  setx /M path "%path%;$(readlink -f ../bin/poetry.bat)"
+
+  assertEquals "$poetry_install_stdout" "$(poetry install)"
+}
+
+test_install_from_bat() {
+  { printf '%s' "$pyproject_content"
+  } > pyproject.toml
+
+  assertEquals "Installing dependencies from lock file
+
+No dependencies to install or update" "$(../bin/poetry.bat install)"
 }
 
 # Load and run shUnit2.
