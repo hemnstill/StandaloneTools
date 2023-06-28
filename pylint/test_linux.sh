2b8a1323d04c4b75a40ae89ea7b820454f40e5b5
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-06-28 23:40:43 +0400

    pylint 2.17.4 python 3.11.3 (#73)

--- pylint/test_linux.sh
+++ pylint/test_linux.sh
@@ -3,9 +3,9 @@
 ../.tools/install_alpine_glibc.sh
 
 test_version() {
-  assertEquals "pylint 2.17.0
-astroid 2.15.0
-Python 3.11.1 (main, Jan 16 2023, 22:40:32) [Clang 15.0.7 ]" "$(../bin/pylint.sh --version)"
+  assertEquals "pylint 2.17.4
+astroid 2.15.5
+Python 3.11.3 (main, May  7 2023, 19:33:53) [Clang 16.0.3 ]" "$(../bin/pylint.sh --version)"
 }
 
 # Load and run shUnit2.
