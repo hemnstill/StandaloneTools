05538631c2597de2b11dd4e0a6062fb21ade63bf
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-07-08 12:00:42 +0400

    enable-legacy

--- openssl/build_mingw.sh
+++ openssl/build_mingw.sh
@@ -30,7 +30,7 @@ echo "::endgroup::"
 
 echo "::group::build"
 
-./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- no-tests no-shared enable-md2 enable-rc5
+./Configure mingw64 --cross-compile-prefix=x86_64-w64-mingw32- no-tests no-shared enable-legacy
 make
 
 echo "::endgroup::"
