dca186fe9585242d7434b8a553a942b699c20118
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-07-23 23:31:56 +0400

    wget

--- ripunzip/build_musl.sh
+++ ripunzip/build_musl.sh
@@ -5,16 +5,30 @@ set -e
 echo "::group::install deps"
 
 apk update
-apk add --no-cache alpine-sdk curl rustup
-
-echo "::endgroup::"
+apk add --no-cache alpine-sdk curl wget rustup
 
 rustup-init -y
 
 . "$HOME/.cargo/env"
 
-rustc --version
-cargo --version
+echo "::endgroup::"
 
+tool_name="ripunzip"
+tool_version="0.1.0"
+self_name="$tool_name-$tool_version"
+release_version_dirpath="$dp0/release/$self_name"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/google/ripunzip/archive/refs/heads/main.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+"$bsdtar" -xf "tool-$tool_version.tar.gz" && cd "tool-$tool_version"
+
+echo "::endgroup::"
 
 
