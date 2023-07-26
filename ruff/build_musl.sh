f43c3581e60baf5bb4aa844e8e7d54ba34ee5608
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-07-26 13:24:30 +0700

    ruff.yml (#81)
    https://github.com/astral-sh/ruff/releases/tag/v0.0.280

---
+++ ruff/build_musl.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk musl-dev gcc curl wget rustup pkgconfig openssl-dev
+
+rustup-init -y
+. "$HOME/.cargo/env"
+
+echo "::endgroup::"
+
+tool_name="ruff"
+tool_version="0.0.280"
+self_name="build-$tool_name-$tool_version"
+self_toolset_name="build-musl"
+release_version_dirpath="$dp0/release/$self_name"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/astral-sh/ruff/archive/refs/tags/v$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+"$bsdtar" -xf "tool-$tool_version.tar.gz" && cd "$tool_name-$tool_version"
+
+cargo build --target x86_64-unknown-linux-musl --release
+
+cp -f "./target/x86_64-unknown-linux-musl/release/$tool_name" "$release_version_dirpath/"
+
+echo "::endgroup::"
+
+cd "$release_version_dirpath"
+strip "$tool_name"
+chmod +x "$tool_name"
+
+{ printf '%s
+
+### %s
+SHA-256: %s
+
+' "$("./$tool_name" --version)" "$self_toolset_name.tar.gz" "$(sha256sum $tool_name)"
+} > "$self_toolset_name.md"
+
+cat "$self_toolset_name.md"
+
+tar -czvf "../$self_toolset_name.tar.gz" .
