404e5711af9bd21dd4929cfed883d02796344301
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-05 23:00:09 +0400

    try alpine-musl

---
+++ 7-Zip-zstd/build_musl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+tool_name="7-Zip-zstd"
+tool_version="22.01-v1.5.5-R3"
+self_toolset_name="build-mingw"
+release_version_dirpath="$dp0/release/build"
+
+tool_dirpath="$dp0/release/7-Zip-zstd-$tool_version"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/mcmilk/7-Zip-zstd/archive/refs/tags/v$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+tar -xf "tool-$tool_version.tar.gz" && cd "$tool_dirpath"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+root="$tool_dirpath/CPP/7zip"
+
+cd "$root/Bundles/Alone2"
+make -j -f makefile.gcc
+cp -f "./x64/7zz" "$release_version_dirpath/"
+
+echo "::endgroup::"
+
+cd "$release_version_dirpath"
+
+{ printf '### %s
+%s
+
+%s
+
+' "$(./7zz | head -2)" "$self_toolset_name.tar.gz" "$download_url"
+} > "$self_toolset_name.md"
+
+cat "$self_toolset_name.md"
+
+"$bsdtar" -czvf "../$self_toolset_name.tar.gz" .
