7ec543db432e1ab8ced8a4475ad6c378621ad899
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-05 22:23:42 +0400

    ls

--- 7-Zip-zstd/build_mingw.sh
+++ 7-Zip-zstd/build_mingw.sh
@@ -13,6 +13,9 @@ mkdir -p "$release_version_dirpath" && cd "$dp0/release"
 download_url="https://github.com/mcmilk/7-Zip-zstd/archive/refs/tags/v$tool_version-R3.tar.gz"
 echo "::group::prepare sources $download_url"
 
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
 mkdir -p "$dp0/release" && cd "$dp0/release"
 wget "$download_url" -O "tool-$tool_version.tar.gz"
 tar -xf "tool-$tool_version.tar.gz" && cd "7-Zip-zstd-$tool_version-R3"
@@ -27,3 +30,15 @@ nmake
 
 echo "::endgroup::"
 
+ls -R
+
+{ printf '### %s
+SHA-256: %s
+%s
+
+' "$self_toolset_name.tar.gz" "$(sha256sum $tool_name)" "$download_url"
+} > "$self_toolset_name.md"
+
+cat "$self_toolset_name.md"
+
+"$bsdtar" -czvf "../$self_toolset_name.tar.gz" .
