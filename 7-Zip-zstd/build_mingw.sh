c753980b25969f6d17a1ec94de72c30d3e7a93a9
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-05 22:11:36 +0400

    nmake Format7zF

--- 7-Zip-zstd/build_mingw.sh
+++ 7-Zip-zstd/build_mingw.sh
@@ -6,7 +6,7 @@ tool_name="7-Zip-zstd"
 tool_version="22.01-v1.5.5"
 self_toolset_name="build-mingw"
 self_name="$tool_name-$tool_version"
-release_version_dirpath="$dp0/release/$self_name"
+release_version_dirpath="$dp0/release/build"
 
 mkdir -p "$release_version_dirpath" && cd "$dp0/release"
 
@@ -14,14 +14,16 @@ download_url="https://github.com/mcmilk/7-Zip-zstd/archive/refs/tags/v$tool_vers
 echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
-wget "$download_url" -O "libarchive-$tool_version.tar.gz"
-tar -xf "libarchive-$tool_version.tar.gz" && cd "libarchive-$tool_version"
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+tar -xf "tool-$tool_version.tar.gz" && cd "7-Zip-zstd-$tool_version-R3"
 
 echo "::endgroup::"
 
+
 echo "::group::build"
 
-
+cd "./CPP/7zip/Bundles/Format7zF"
+nmake
 
 echo "::endgroup::"
 
