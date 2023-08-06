3cacb96518868312b14b7fca8a5a2fc3fe3c3e94
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-06 21:37:56 +0400

    xz

--- 7-Zip/build_musl.sh
+++ 7-Zip/build_musl.sh
@@ -5,7 +5,7 @@ set -e
 echo "::group::install deps"
 
 apk update
-apk add --no-cache alpine-sdk make linux-headers mingw-w64-gcc
+apk add --no-cache alpine-sdk make linux-headers xz
 
 echo "::endgroup::"
 
