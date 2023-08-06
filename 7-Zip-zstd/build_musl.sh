a6b59845098887bb146e48a1255757735605ccd2
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-06 10:12:18 +0400

    ubuntu

--- 7-Zip-zstd/build_musl.sh
+++ 7-Zip-zstd/build_musl.sh
@@ -36,9 +36,6 @@ root="$tool_dirpath/CPP/7zip"
 
 cd "$root/Bundles/Alone2"
 make -j -f makefile.gcc DISABLE_RAR=1
-#make -j -f ../../cmpl_clang.mak
-
-ls -R
 
 cp -f "./_o/7zz" "$release_version_dirpath/"
 
