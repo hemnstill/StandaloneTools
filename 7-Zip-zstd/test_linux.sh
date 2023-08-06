dfd05fb23f34cd115a27c071eb66db671e639347
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-06 10:06:04 +0400

    test

---
+++ 7-Zip-zstd/test_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+testVersion() {
+  assertEquals "
+7-Zip 22.01 ZS v1.5.5 R3 (x64) : Copyright (c) 1999-2022 Igor Pavlov, 2016-2023 Tino Reichardt : 2023-06-18" "$(../bin/7zz | head -2)"
+}
+
+# Load and run shUnit2.
+source "../.tests/shunit2/shunit2"
