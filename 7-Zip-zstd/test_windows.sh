bb64530c658bf65632bf5451e3f966d2d5e3172a
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-06 10:23:26 +0400

    7zz.exe

--- 7-Zip-zstd/test_windows.sh
+++ 7-Zip-zstd/test_windows.sh
@@ -2,7 +2,7 @@
 
 testVersion() {
   assertEquals "
-7-Zip (z) 23.01 (x64) : Copyright (c) 1999-2023 Igor Pavlov : 2023-06-20" "$(../bin/7z.exe | dos2unix | head -2)"
+7-Zip (z) 23.01 (x64) : Copyright (c) 1999-2023 Igor Pavlov : 2023-06-20" "$(../bin/7zz.exe | dos2unix | head -2)"
 }
 
 # Load and run shUnit2.
