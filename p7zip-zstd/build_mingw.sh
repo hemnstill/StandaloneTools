15b26db48afd1d98a921be075c963ab710326bd5
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-11 19:10:00 +0400

    p7zip-zstd.yml

---
+++ p7zip-zstd/build_mingw.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+tool_name="p7zip-zstd"
+tool_version="17.05"
+self_toolset_name="build-mingw"
+release_version_dirpath="$dp0/release/build"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/p7zip-project/p7zip/archive/refs/tags/v17.05.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+# wget failed: ssl_client: TLS error from peer (alert code 80): 80
+curl --location "$download_url" --output "tool-$tool_version.tar.xz"
+"$bsdtar" -xf "tool-$tool_version.tar.xz"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+cd "$dp0/release/CPP/7zip/Bundles/Alone2"
+nmake
+cp -f "./x64/7zz.exe" "$release_version_dirpath/"
+
+echo "::endgroup::"
+
+cd "$release_version_dirpath"
+
+strip 7zz.exe
+
+{ printf '### %s
+%s
+
+SHA-256: %s
+
+<details>
+  <summary>7zz i</summary>
+
+```
+%s
+```
+</details>
+
+%s
+
+' "$self_toolset_name.tar.gz" "$(./7zz.exe | head -2)" "$(sha256sum 7zz.exe)" "$(./7zz.exe i)" "$download_url"
+} > "$self_toolset_name.md"
+
+cat "$self_toolset_name.md"
+
+"$bsdtar" -czvf "../$self_toolset_name.tar.gz" .
---
+++ p7zip-zstd/test_windows.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+testVersion() {
+  assertEquals "
+p7zip-zstd (z) 17.05 (x64) : Copyright (c) 1999-2023 Igor Pavlov : 2023-06-20" "$(../bin/7zz.exe | dos2unix | head -2)"
+}
+
+# Load and run shUnit2.
+source "../.tests/shunit2/shunit2"
