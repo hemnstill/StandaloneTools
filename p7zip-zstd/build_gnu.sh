bafead74fc78f9e4edfaedec61a51d21b4851855
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-14 20:17:46 +0400

    vf

--- p7zip-zstd/build_gnu.sh
+++ p7zip-zstd/build_gnu.sh
@@ -36,8 +36,8 @@ echo "::group::build"
 
 cd "$source_dirpath/CPP/7zip/Bundles/Alone2"
 make -f makefile.gcc
-cp -rv "./_o/bin/" "$release_version_dirpath/"
-cp -rv "./_o/lib/" "$release_version_dirpath/"
+cp -vf "./_o/bin/7zz" "$release_version_dirpath/"
+cp -vr "./_o/lib/" "$release_version_dirpath/"
 
 ls -R "./_o"
 
