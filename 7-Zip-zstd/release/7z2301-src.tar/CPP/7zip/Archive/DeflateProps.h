1c866321d9ead0eb30cd2c27b7be444a94c1bc99
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-14 23:16:19 +0400

    -7z2301-src

--- 7-Zip-zstd/release/7z2301-src.tar/CPP/7zip/Archive/DeflateProps.h
+++
@@ -1,6 +0,0 @@
-// DeflateProps.h
-
-#ifndef ZIP7_INC_DEFLATE_PROPS_H
-#define ZIP7_INC_DEFLATE_PROPS_H
-
-#endif
--- 7-Zip-zstd/release/7z2301-src.tar/CPP/7zip/Compress/ImplodeHuffmanDecoder.h
+++
@@ -1,6 +0,0 @@
-// ImplodeHuffmanDecoder.h
-
-#ifndef ZIP7_INC_IMPLODE_HUFFMAN_DECODER_H
-#define ZIP7_INC_IMPLODE_HUFFMAN_DECODER_H
-
-#endif
