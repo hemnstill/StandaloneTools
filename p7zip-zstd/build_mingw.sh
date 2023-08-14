5f972b4d10baa8830e6819558056b8e61c3b81ae
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-14 07:27:33 +0400

    try windows

--- p7zip-zstd/build_mingw.sh
+++ p7zip-zstd/build_mingw.sh
@@ -6,11 +6,12 @@ tool_name="p7zip-zstd"
 tool_version="22.00"
 self_toolset_name="build-mingw"
 release_version_dirpath="$dp0/release/build"
-source_dirpath="$dp0/release/p7zip-p7zip$tool_version"
+commit_id=36f6b7422234c8c48d3eaaa80a0e8ceb1180f081
+source_dirpath="$dp0/release/p7zip-$commit_id"
 
 mkdir -p "$release_version_dirpath" && cd "$dp0/release"
 
-download_url="https://github.com/p7zip-project/p7zip/archive/refs/heads/p7zip22.00.tar.gz"
+download_url="https://github.com/p7zip-project/p7zip/archive/$commit_id.tar.gz"
 echo "::group::prepare sources $download_url"
 
 "$dp0/../.tools/download_bsdtar.sh"
