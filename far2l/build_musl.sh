0cca550b52c4dd655fadd35b519e9cee74729c81
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-08-19 00:15:19 +0700

    far2l 2.5.2 (#86)
    alpine version 3.16

--- far2l/build_musl.sh
+++ far2l/build_musl.sh
@@ -28,7 +28,7 @@ apk add --no-cache \
 echo "::endgroup::"
 
 tool_name="far2l"
-tool_version="2.5.1"
+tool_version="2.5.2"
 self_name="$tool_name-$tool_version"
 self_toolset_name="build-musl"
 self_url="https://github.com/hemnstill/StandaloneTools/releases/download/$self_name/$self_toolset_name.tar.gz"
