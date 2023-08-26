780f87f94a8f22e7379b4333cead79fa834c6a16
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-26 17:35:54 +0400

    mysql cmake alpine build

---
+++ mysql/build_musl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk linux-headers cmake
+
+echo "::endgroup::"
+
+tool_name="mysql"
+tool_version="8.0.34"
+self_toolset_name="build-musl"
+release_version_dirpath="$dp0/release/build"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/mysql/mysql-server/archive/refs/tags/$tool_name-$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+"$bsdtar" -xf "tool-$tool_version.tar.gz" && cd "mysql-server-mysql-$tool_version"
+
+cmake --build . --config Release
