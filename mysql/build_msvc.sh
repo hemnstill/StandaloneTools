c886fbfb6f93fb0d813e83d03f3ca9cbd5c637fd
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-26 20:56:47 +0400

    -win64

--- mysql/build_msvc.sh
+++ mysql/build_msvc.sh
@@ -35,7 +35,7 @@ cmake . -LH \
   -DWITH_UNIT_TESTS=0 \
   -DWITH_BUILD_ID=0 \
   -DREPRODUCIBLE_BUILD=1 \
-  -G "Visual Studio 16 2019" -A "Win64" \
+  -G "Visual Studio 16 2019" \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_CONFIG=mysql_release
 
