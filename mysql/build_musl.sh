8c963649f6cd1e9859e1c9f437ab8391068078e8
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-26 18:06:43 +0400

    boost-dev

--- mysql/build_musl.sh
+++ mysql/build_musl.sh
@@ -6,7 +6,7 @@ echo "::group::install deps"
 
 apk update
 apk add --no-cache alpine-sdk linux-headers cmake
-apk add --no-cache libaio-dev
+apk add --no-cache libaio-dev boost-dev
 
 echo "::endgroup::"
 
