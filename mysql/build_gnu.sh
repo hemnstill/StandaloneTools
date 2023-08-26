a422ae9f050da2b68552134c4572a27e4545242c
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-27 00:48:23 +0400

    build_gnu.sh

---
+++ mysql/build_gnu.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "::group::install deps"
+
+apt update
+apt install -y build-essential cmake
+apt install -y libaio-dev libudev-dev openldap-dev openssl-dev ncurses-dev zstd-dev
+
+echo "::endgroup::"
+
+tool_name="mysql"
+tool_version="8.0.33"
+self_toolset_name="build-gnu"
+release_version_dirpath="$dp0/release/build"
+
+mkdir -p "$release_version_dirpath" && cd "$dp0/release"
+
+download_url="https://github.com/mysql/mysql-server/archive/refs/tags/$tool_name-$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+"$dp0/../.tools/download_bsdtar.sh"
+bsdtar="$dp0/release/bsdtar"
+
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+"$bsdtar" -xf "tool-$tool_version.tar.gz" && cd "mysql-server-mysql-$tool_version"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+cmake . -LH \
+  -DDOWNLOAD_BOOST=1 \
+  -DWITH_BOOST=./boost \
+  -DFORCE_INSOURCE_BUILD=1 \
+  -DWITHOUT_SERVER=1 \
+  -DBUILD_STATIC=1 \
+  -DBUILD_SHARED_LIBS=0 \
+  -DMYSQLCLIENT_STATIC_LINKING=1 \
+  -DDISABLE_SHARED=1 \
+  -DNCURSES_STATIC=1 \
+  -DBUNDLE_DEPENDENCIES=1 \
+  -DWITH_UNIT_TESTS=0 \
+  -DWITH_BUILD_ID=0 \
+  -DREPRODUCIBLE_BUILD=1 \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_CONFIG=mysql_release
+
+cmake --build . --config Release
+
+cp -rf "./runtime_output_directory/." "$release_version_dirpath/"
+
+
+cd "$release_version_dirpath"
+
+find . -mindepth 1 -maxdepth 1 -name '*test' -exec rm -f "{}" \;
+
+find . -mindepth 1 -maxdepth 1 -exec strip "{}" \;
+
+{ printf '### %s
+%s
+
+%s
+' "$self_toolset_name.tar.gz" "$("./$tool_name" --version)" "$(sha256sum ./*)"
+} > "$self_toolset_name.md"
+
+cat "$self_toolset_name.md"
+
+tar -czvf "../$self_toolset_name.tar.gz" .
+
+echo "::endgroup::"
