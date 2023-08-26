8092d758793c84cf4f8da3e80e93cb24976b2484
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-26 20:34:35 +0400

    rm test binaries

--- mysql/build_musl.sh
+++ mysql/build_musl.sh
@@ -47,11 +47,12 @@ cmake . -LH \
 
 make
 
-ls -R
-
 cp -rf "./utilities/runtime_output_directory/." "$release_version_dirpath/"
 
+
 cd "$release_version_dirpath"
+
+find . -mindepth 1 -maxdepth 1 -name '*test' -exec rm -f "{}" \;
 
 { printf '%s
 ### %s
