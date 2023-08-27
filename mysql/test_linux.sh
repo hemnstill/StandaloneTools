7ffe3974c5804147d2d9ff1d07877a138cf661b0
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-27 12:42:31 +0400

    ldd ../bin/mysql

--- mysql/test_linux.sh
+++ mysql/test_linux.sh
@@ -2,6 +2,8 @@
 
 ../.tools/install_alpine_glibc.sh
 
+ldd ../bin/mysql
+
 testVersion() {
   assertEquals "../bin/mysql  Ver 8.0.33 for Linux on x86_64 (Source distribution)" "$(../bin/mysql --version)"
 }
