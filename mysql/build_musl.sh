e1d39205bac06f9a461b7b9b89aa851b6f37193c
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-27 11:31:24 +0400

    -o

--- mysql/build_musl.sh
+++ mysql/build_musl.sh
@@ -46,7 +46,7 @@ cmake . \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_CONFIG=mysql_release
 
-find . -type f -mindepth 2 -maxdepth 4 -name "link.txt" ! -name "*test*" -exec echo "{}" \; \
+find . -type f -mindepth 2 -maxdepth 4 -name "link.txt" -o ! -name "*test.dir*" -exec echo "{}" \; \
   -exec sed -i -e 's@/usr/lib/libssl.so@/usr/lib/libssl.a@g' \
   -e 's@/usr/lib/libcrypto.so@/usr/lib/libcrypto.a@g' \
   -e 's@-lssl -lcrypto -lncurses@-lssl -lcrypto -lncurses -static@g' \
