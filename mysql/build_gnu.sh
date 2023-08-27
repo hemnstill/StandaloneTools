0e4dd0b4bc19c5d5ad0c8c19d5b3ad212449f239
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-27 22:39:13 +0400

    checks

--- mysql/build_gnu.sh
+++ mysql/build_gnu.sh
@@ -48,7 +48,7 @@ cmake . \
 find . -type f -mindepth 2 -maxdepth 5 -name "link.txt" \
   -exec echo replace '.so' "{}" \; \
   -exec sed -i -e 's@/usr/lib/x86_64-linux-gnu/libssl.so@/usr/lib/x86_64-linux-gnu/libssl.a@g' \
-               -e 's@/usr/lib/x86_64-linux-gnu/libcrypto.so@/usr/lib/x86_64-linux-gnu/libcrypto.a@g' "{}" \;
+            -e 's@/usr/lib/x86_64-linux-gnu/libcrypto.so@/usr/lib/x86_64-linux-gnu/libcrypto.a@g' "{}" \;
 
 cmake --build . --config Release
 
