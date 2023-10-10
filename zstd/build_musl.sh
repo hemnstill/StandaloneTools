94839299919a651ad14665ec6034bcc4ef055ed5
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-10-10 14:45:17 +0400

    zstd

--- zstd/build_musl.sh
+++ zstd/build_musl.sh
@@ -17,8 +17,8 @@ download_url="https://github.com/facebook/zstd/archive/refs/tags/v$tool_version.
 echo "::group::prepare sources $download_url"
 
 mkdir -p "$dp0/release" && cd "$dp0/release"
-wget "$download_url" -O "libarchive-$tool_version.tar.gz"
-tar -xf "libarchive-$tool_version.tar.gz" && cd "libarchive-$tool_version"
+wget "$download_url" -O "tool-$tool_version.tar.gz"
+tar -xf "tool-$tool_version.tar.gz" && cd "$tool_name-$tool_version"
 
 echo "::endgroup::"
 
