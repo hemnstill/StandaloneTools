20c83eb059e7db71d7375a29229438dc806c2c07
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-10-10 23:07:01 +0400

    ..

--- zstd/build_mingw.sh
+++ zstd/build_mingw.sh
@@ -25,8 +25,8 @@ git clone --depth 1 --branch v1.2.11 https://github.com/madler/zlib
 make -C zlib -f win32/Makefile.gcc libz.a
 
 cd "$dp0/release/$tool_name-$tool_version"
-export CPPFLAGS=-I./zlib
-export LDFLAGS=./zlib/libz.a
+export CPPFLAGS=-I../zlib
+export LDFLAGS=../zlib/libz.a
 make -j allzstd MOREFLAGS=-static V=1
 
 cp -f "./$tool_name.exe" "$dp0/release/build/"
