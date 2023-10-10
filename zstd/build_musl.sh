d16a49ead146473628c09e3ff182350b47755638
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-10-10 14:27:31 +0400

    make zstd

---
+++ zstd/build_musl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+set -e
+
+echo "::group::install deps"
+
+apk update
+apk add --no-cache alpine-sdk zlib-dev zlib-static xz-dev zstd-dev zstd-static
+
+echo "::endgroup::"
+
+tool_name="zstd"
+tool_version="1.5.5"
+self_toolset_name="build-musl"
+
+download_url="https://github.com/facebook/zstd/archive/refs/tags/v$tool_version.tar.gz"
+echo "::group::prepare sources $download_url"
+
+mkdir -p "$dp0/release" && cd "$dp0/release"
+wget "$download_url" -O "libarchive-$tool_version.tar.gz"
+tar -xf "libarchive-$tool_version.tar.gz" && cd "libarchive-$tool_version"
+
+echo "::endgroup::"
+
+echo "::group::build"
+
+make
