d353bea150776bb56b84d677e760351dd4d460be
Рахимов Рустам (Rakhimov_RR) <Rakhimov_RR@Directum.ru>
2024-02-16 14:34:52 +0400

    tests up

--- pylint/test_linux.sh
+++ pylint/test_linux.sh
@@ -4,7 +4,7 @@
 
 test_version() {
   assertEquals "pylint 3.0.3
-astroid 2.15.5
+astroid 3.0.3
 Python 3.11.3 (main, May  7 2023, 19:33:53) [Clang 16.0.3 ]" "$(../bin/pylint.sh --version)"
 }
 
--- pylint/test_windows.sh
+++ pylint/test_windows.sh
@@ -2,7 +2,7 @@
 
 test_version() {
   assertEquals "pylint 3.0.3
-astroid 2.15.5
+astroid 3.0.3
 Python 3.11.3 (main, May  7 2023, 18:40:36) [MSC v.1929 64 bit (AMD64)]" "$(../bin/pylint.bat --version | dos2unix )"
 }
 
