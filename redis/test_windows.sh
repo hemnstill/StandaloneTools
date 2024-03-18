92f1f6582915898d5485d2d6ff8d240f496f321b
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2024-03-19 01:08:55 +0400

    recompile redis 7.2.* with msys2 (#99)
    * disable optimizations with `CFLAGS="-Wno-char-subscripts -O0"`
    * add `redis-server.exe` version tests

--- redis/test_windows.sh
+++ redis/test_windows.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "redis-cli 7.2.3" "$(../bin/redis-cli.exe --version)"
+  assertEquals "redis-cli 7.2.3" "$(../bin/redis-cli.exe --version | head -c 15)"
+}
+
+testServerVersion() {
+  assertEquals "Redis server v=7.2.3" "$(../bin/redis-server.exe --version | head -c 20)"
 }
 
 # Load and run shUnit2.
