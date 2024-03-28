c5bd16a667c505c6326cbb3ec693a29a7b6788bc
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2024-03-28 16:42:49 +0400

    Poetry 1.8.2 with Python 3.12.2 (#101)

--- poetry/test_linux.sh
+++ poetry/test_linux.sh
@@ -18,7 +18,7 @@ authors = []
 readme = "README.md"
 
 [tool.poetry.dependencies]
-python = "3.12.1"
+python = "3.12.2"
 requests = "2.28.2"
 mysqlclient = "2.2.1"
 
@@ -32,32 +32,32 @@ Resolving dependencies...
 
 Package operations: 6 installs, 0 updates, 0 removals
 
-  • Installing certifi (2023.11.17)
-  • Installing charset-normalizer (3.3.2)
-  • Installing idna (3.6)
-  • Installing urllib3 (1.26.18)
-  • Installing mysqlclient (2.2.1)
-  • Installing requests (2.28.2)
+  - Installing certifi (2024.2.2)
+  - Installing charset-normalizer (3.3.2)
+  - Installing idna (3.6)
+  - Installing urllib3 (1.26.18)
+  - Installing mysqlclient (2.2.1)
+  - Installing requests (2.28.2)
 
 Writing lock file'
 
 test_version() {
-  assertEquals "Poetry (version 1.7.1)" "$(../bin/poetry.sh --version)"
+  assertEquals "Poetry (version 1.8.2)" "$(../bin/poetry.sh --version)"
 }
 
 test_version_plugins() {
   assertEquals "
-  • poetry-plugin-sort (0.2.1) Poetry plugin to sort the dependencies alphabetically
+  - poetry-plugin-sort (0.2.1) Poetry plugin to sort the dependencies alphabetically
       1 application plugin
 
       Dependencies
         - poetry (>=1.2.0,<2.0.0)
 
-  • poetry-plugin-export (1.6.0) Poetry plugin to export the dependencies to various formats
+  - poetry-plugin-export (1.7.1) Poetry plugin to export the dependencies to various formats
       1 application plugin
 
       Dependencies
-        - poetry (>=1.6.0,<2.0.0)
+        - poetry (>=1.8.0,<2.0.0)
         - poetry-core (>=1.7.0,<2.0.0)" "$(../bin/poetry.sh self show plugins)"
 }
 
