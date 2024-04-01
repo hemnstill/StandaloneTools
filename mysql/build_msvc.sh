472c53a168b3245b1fc82b89c0661cacd3d5caaf
Reshetnikov_AS <hemn.still@gmail.com>
2024-04-01 20:22:18 +0400

    WITH_SSL

--- mysql/build_msvc.sh
+++ mysql/build_msvc.sh
@@ -27,6 +27,7 @@ echo "::endgroup::"
 echo "::group::build"
 
 cmake . \
+  -DWITH_SSL=C:/vcpkg/packages/openssl_x64-windows-static-md \
   -DDOWNLOAD_BOOST=1 \
   -DWITH_BOOST=./boost \
   -DFORCE_INSOURCE_BUILD=1 \
