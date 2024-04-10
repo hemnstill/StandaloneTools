dff1c2eaf5dc81b5dba99ef90b8f4c1717bc2938
Абрамов Михаил (Abramov_MA) <Abramov_MA@directum.ru>
2024-04-10 08:59:28 +0400

    win-test up

--- bsdtar/test_windows.sh
+++ bsdtar/test_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "bsdtar 3.7.3 - libarchive 3.7.3 zlib/1.2.12 liblzma/5.2.5 libzstd/1.5.2 " "$(../bin/bsdtar.exe --version)"
+  assertEquals "bsdtar 3.7.3 - libarchive 3.7.3 zlib/1.3 liblzma/5.4.4 libzstd/1.5.6 " "$(../bin/bsdtar.exe --version)"
 }
 
 # Load and run shUnit2.
