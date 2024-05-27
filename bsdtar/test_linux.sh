7a9db846ae69759a58cdc4c1e979ffdb710f7739
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2024-05-27 11:22:02 -0700

    bsdtar 3.7.3 -> 3.7.4 (#109)

--- bsdtar/test_linux.sh
+++ bsdtar/test_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "bsdtar 3.7.3 - libarchive 3.7.3 zlib/1.3.1 liblzma/5.4.5 libzstd/1.5.6 " "$(../bin/bsdtar --version)"
+  assertEquals "bsdtar 3.7.4 - libarchive 3.7.4 zlib/1.3.1 liblzma/5.4.5 libzstd/1.5.6 " "$(../bin/bsdtar --version)"
 }
 
 # Load and run shUnit2.
