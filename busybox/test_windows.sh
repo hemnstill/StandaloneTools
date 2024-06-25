57d3e52e25a148c8186fb8afd7218c11a4d90097
Reshetnikov Alexandr <hemn.still@gmail.com>
2024-06-25 23:12:41 +0400

    windows tests up

--- busybox/test_windows.sh
+++ busybox/test_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "tar (busybox) 1.36.0.git" "$(../bin/busybox.exe tar --version | head -1)"
+  assertEquals "tar (busybox) 1.37.0.git-5398-g89ae34445" "$(../bin/busybox.exe tar --version | head -1)"
 }
 
 # Load and run shUnit2.
