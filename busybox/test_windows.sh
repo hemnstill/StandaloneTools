fe2ccd1ccf51fd6b82ec30b4c567fd2b4f42a9fe
Reshetnikov Alexandr <hemn.still@gmail.com>
2024-06-25 23:54:00 +0400

    try fix exe info

--- busybox/test_windows.sh
+++ busybox/test_windows.sh
@@ -2,6 +2,8 @@
 
 testVersion() {
   assertEquals "tar (busybox) 1.37.0.git-5398-g89ae34445" "$(../bin/busybox.exe tar --version | head -1)"
+
+  "../bin/busybox.exe" > ./release/test-windows.md
 }
 
 # Load and run shUnit2.
