e91c54b84a03f52173fc246cc3140993f8c8125c
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2024-09-25 11:07:32 +0400

    bsdtar 3.7.6 (#123)

--- bsdtar/build_mingw.sh
+++ bsdtar/build_mingw.sh
@@ -9,7 +9,7 @@ choco install mingw
 echo "::endgroup::"
 
 tool_name="bsdtar.exe"
-tool_version="3.7.5"
+tool_version="3.7.6"
 self_toolset_name="build-mingw"
 
 download_url="https://github.com/libarchive/libarchive/releases/download/v$tool_version/libarchive-$tool_version.tar.gz"
--- bsdtar/test_windows.sh
+++ bsdtar/test_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "bsdtar 3.7.5 - libarchive 3.7.5 zlib/1.3 liblzma/5.4.4 libzstd/1.5.6 " "$(../bin/bsdtar.exe --version)"
+  assertEquals "bsdtar 3.7.6 - libarchive 3.7.6 zlib/1.3 liblzma/5.4.4 libzstd/1.5.6 " "$(../bin/bsdtar.exe --version)"
 }
 
 # Load and run shUnit2.
