4369f99bb3e96543760fa2fe5bc77d495448690a
Reshetnikov_AS <hemn.still@gmail.com>
2024-10-16 10:07:16 +0400

    3.7.6 → 3.7.7

--- bsdtar/build_musl.sh
+++ bsdtar/build_musl.sh
@@ -13,7 +13,7 @@ apk add --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main a
 echo "::endgroup::"
 
 tool_name="bsdtar"
-tool_version="3.7.6"
+tool_version="3.7.7"
 self_toolset_name="build-musl"
 
 download_url="https://github.com/libarchive/libarchive/releases/download/v$tool_version/libarchive-$tool_version.tar.gz"
