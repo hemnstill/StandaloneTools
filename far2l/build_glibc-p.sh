b943f503a0fdd38eff234db0d588e118576ca61c
Reshetnikov Alexandr <hemn.still@gmail.com>
2025-03-09 20:23:37 +0400

    without colorer

--- far2l/build_glibc-p.sh
+++ far2l/build_glibc-p.sh
@@ -49,7 +49,12 @@ echo "::endgroup::"
 
 echo "::group::build"
 
+without_plugins="\
+-DCOLORER=no \
+"
+
 cmake_command=$(printf 'cmake -DUSEWX=no -DUSEUCD=no -DCMAKE_EXE_LINKER_FLAGS="%s" -DCMAKE_BUILD_TYPE=Release .' \
+  "$without_plugins" \
   "-l:libuchardet.a -static-libstdc++ -static-libgcc")
 echo ">> $cmake_command"
 eval "$cmake_command"
