195471f259a5889392cb9425e9182a6a0fae16d9
Reshetnikov Alexandr <hemn.still@gmail.com>
2025-03-29 14:19:24 +0400

    bsdtar 3.7.7 -> 3.7.8

--- bsdtar/test_windows.sh
+++ bsdtar/test_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "bsdtar 3.7.7 - libarchive 3.7.7 zlib/1.3 liblzma/5.4.4 libzstd/1.5.6 " "$(../bin/bsdtar.exe --version)"
+  assertEquals "bsdtar 3.7.8 - libarchive 3.7.8 zlib/1.3 liblzma/5.4.4 libzstd/1.5.6 " "$(../bin/bsdtar.exe --version)"
 }
 
 # Load and run shUnit2.
