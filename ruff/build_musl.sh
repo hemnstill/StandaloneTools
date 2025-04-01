26c4baad3ced301f59161dd790ccfe0f2a1df247
Reshetnikov Alexandr <hemn.still@gmail.com>
2025-04-02 02:01:08 +0400

    copy linux binaries

--- ruff/build_musl.sh
+++ ruff/build_musl.sh
@@ -31,7 +31,7 @@ wget "$download_url" -O "tool-$tool_version.tar.gz"
 
 cargo build --target x86_64-unknown-linux-musl --release
 
-cp -f "./target/x86_64-unknown-linux-musl/release/$tool_name" "$release_version_dirpath/"
+find "./target/x86_64-unknown-linux-musl/release" -maxdepth 1 -type f ! -name "*.*" -exec cp -f {} "$release_version_dirpath/" \;
 
 echo "::endgroup::"
 
