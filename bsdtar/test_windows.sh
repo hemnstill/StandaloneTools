4a3b450ce9461cbcb9f1cc5a0ecc1cfa4ee52c1a
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2025-07-06 15:37:01 +0200

    bsdtar 3.8.1 (#143)

--- bsdtar/test_windows.sh
+++ bsdtar/test_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  assertEquals "bsdtar 3.8.0 - libarchive 3.8.0 zlib/1.3 liblzma/5.6.3 libzstd/1.5.7 cng/1.0 libb2/bundled " "$(../bin/bsdtar.exe --version)"
+  assertEquals "bsdtar 3.8.1 - libarchive 3.8.1 zlib/1.3 liblzma/5.6.3 libzstd/1.5.7 cng/1.0 libb2/bundled " "$(../bin/bsdtar.exe --version)"
 }
 
 testZipEncrypt() {
