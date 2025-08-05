7a80f43bb0987240c2b68bf20f8421663981459b
Reshetnikov Alexandr <hemn.still@gmail.com>
2025-08-05 22:56:34 +0400

    try ty

--- ruff/build_mingw.sh
+++ ruff/build_mingw.sh
@@ -13,8 +13,8 @@ rustup-init -y
 echo "::endgroup::"
 
 tool_name="ruff"
-tool_name_knot="red_knot"
-tool_version="0.11.2"
+tool_name_ty="ty"
+tool_version="0.12.7"
 self_name="build-$tool_name-$tool_version"
 self_toolset_name="build-mingw"
 release_version_dirpath="$dp0/release/$self_name"
@@ -34,20 +34,20 @@ cargo fetch
 rustup target add x86_64-pc-windows-gnu
 cargo build --target x86_64-pc-windows-gnu --release
 
-find "./target/x86_64-pc-windows-gnu/release" -maxdepth 1 -type f \( -name "$tool_name.exe" -o -name "$tool_name_knot.exe" \) -exec cp -f {} "$release_version_dirpath/" \;
+find "./target/x86_64-pc-windows-gnu/release" -maxdepth 1 -type f \( -name "$tool_name.exe" -o -name "$tool_name_ty.exe" \) -exec cp -f {} "$release_version_dirpath/" \;
 
 echo "::endgroup::"
 
 cd "$release_version_dirpath"
 
 strip "$tool_name.exe"
-strip "$tool_name_knot.exe"
+strip "$tool_name_ty.exe"
 
 { printf '### %s
 SHA-256: %s
 SHA-256: %s
 
-' "$self_toolset_name.tar.gz" "$(sha256sum $tool_name.exe)" "$(sha256sum $tool_name_knot.exe)"
+' "$self_toolset_name.tar.gz" "$(sha256sum $tool_name.exe)" "$(sha256sum $tool_name_ty.exe)"
 } > "$self_toolset_name.md"
 
 cat "$self_toolset_name.md"
