041d710144558b84297b623e18cb4bbed78c2e1c
Reshetnikov Alexandr <hemn.still@gmail.com>
2026-01-26 14:28:53 +0400

    mysql 8.4.8

--- mysql/test_linux.sh
+++ mysql/test_linux.sh
@@ -3,7 +3,7 @@
 "../.tools/install_alpine_glibc.sh"
 
 testVersion() {
-  assertEquals "../bin/mysql  Ver 8.4.3 for Linux on x86_64 (Source distribution)" "$(../bin/mysql --version)"
+  assertEquals "../bin/mysql  Ver 8.4.8 for Linux on x86_64 (Source distribution)" "$(../bin/mysql --version)"
 }
 
 # Load and run shUnit2.
